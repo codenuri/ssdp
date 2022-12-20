0f0ee603d78fb68c137f09f6a1f19f3683301843
ecourse <smkang@codenuri.co.kr>
2022-12-20 16:14:47 +0900

    Add files via upload

--- DAY2/bind2.cpp
+++ DAY2/bind2.cpp
@@ -11,6 +11,12 @@ void f4(int a, int b, int c, int d)
 void f1(int a) {}
 void f2(int a, int b) {}
 
+class Dialog
+{
+public:
+	void close(int a, double d) { std::cout << "Dialog Close" << std::endl; }
+};
+
 int main()
 {
 	// 함수 포인터는 유연성이 부족합니다
@@ -33,10 +39,18 @@ int main()
 	f(10); // f2(10, 7)
 
 
-	f = std::bind(&f4, ? ? ? ? );
+	f = std::bind(&f4, 7, 3, _1, 9 );
 	f(6); // f4(7, 3, 6, 9) 나오게 해보세요
 
+	// 결국 "f" 에는 인자가 "int 한개" 이상인 모든 종류의 함수를 
+	// 담을수 있습니다. std::bind() 덕분에.
 
+	// 멤버 함수 주소를 담는 방법
+	// => 멤버 함수를 호출하려면 객체가 필요 합니다.
+	Dialog dlg;
+	f = std::bind(&Dialog::close, &dlg, _1, 3.4);
+
+	f(10); // dlg.close(10, 3.4) 의 의미!
 }
 
 
