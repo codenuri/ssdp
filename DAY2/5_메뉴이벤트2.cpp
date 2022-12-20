b2d1e336f03b7d74dd217c72c274f0e928bfd22c
ecourse <smkang@codenuri.co.kr>
2022-12-20 14:59:03 +0900

    Add files via upload

---
+++ DAY2/5_메뉴이벤트2.cpp
@@ -0,0 +1,64 @@
+#include "PopupMenu.h"
+
+// 방법 1. 변하는 것을 가상함수로
+class MenuItem : public BaseMenu
+{
+	int  id;
+public:
+	MenuItem(const std::string& title, int id) : BaseMenu(title), id(id) {}
+
+	void command()
+	{
+		// 메뉴 선택시 공통으로 해야할 일이 있다면 
+		// 여기서 공통작업을 하고
+		// 다시 가상함수 호출.!
+		do_command();
+	}
+
+	virtual void do_command() {}
+};
+
+class FHDMenu : public MenuItem
+{
+public:
+//	FHDMenu(const std::string& title, int id) : MenuItem(title, id) {}
+
+	// 위 생성자는 전달받은 인자를 기반 클래스에 보내기만 합니다.
+	// 이 경우, C++11 부터는 아래 처럼하세요
+	// "생성자 상속" 이라는 문법
+	using MenuItem::MenuItem;  // MenuItem 의 모든 생성자를 상속해 달라.   
+
+	virtual void do_command() override
+	{
+		std::cout << "FHD로 변경하는 코드" << std::endl;
+		_getch();
+	}
+};
+
+class UHDMenu : public MenuItem
+{
+public:
+	using MenuItem::MenuItem;
+
+	virtual void do_command() override
+	{
+		std::cout << "UHD로 변경하는 코드" << std::endl;
+		_getch();
+	}
+};
+
+
+int main()
+{
+	PopupMenu* root = new PopupMenu("ROOT");
+
+	root->add_menu(new FHDMenu("FHD", 11));
+	root->add_menu(new UHDMenu("UHD", 12));
+	
+	root->command();
+}
+
+
+
+
+
