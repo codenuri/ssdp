a722c62c36ca58d6d7e414ccb95d6a4763137b89
ecourse <smkang@codenuri.co.kr>
2022-12-20 15:20:16 +0900

    Add files via upload

--- DAY2/5_메뉴이벤트3.cpp
+++ DAY2/5_메뉴이벤트3.cpp
@@ -1,13 +1,20 @@
 #include "PopupMenu.h"
 
 // 방법 2. 변하는 것을 다른 클래스로!!!
+// => 메뉴 선택시, 인터페이스로 약속된 함수를 호출
+// => java, 안드로이드의 "리스너" 방식
+// => 객체지향 언어에서 가장 널리 사용되는 방식
+
+// 특징 : 여러개 메뉴를 한개의 함수에서 처리하게 되는 경우가 많아지고
+//        커다란 switch-case 를 만들게 된다.
+
 
 // 메뉴 이벤트를 처리하고 싶은 모든 클래스는
 // 반드시 아래 인터페이스를 구현해야 한다.
 
 struct IMenuListener
 {
-	virtual void on_command() = 0;
+	virtual void on_command(int id) = 0;
 	virtual ~IMenuListener() {}
 };
 
@@ -23,7 +30,7 @@ public:
 	void command()
 	{
 		if (ps != nullptr)
-			ps->on_command();
+			ps->on_command(id);
 	}
 };
 //=========================================
@@ -34,9 +41,17 @@ class Camera : public IMenuListener
 public:
 	// ....다양한 멤버들...
 
-	virtual void on_command() override
+	virtual void on_command(int id) override
 	{
 		std::cout << "Camera 객체가 메뉴 이벤트 처리" << std::endl;
+
+		// 수십개의 메뉴를 이곳에서 처리하게 된다면
+		// "거대한 switch ~ case" 를 가지게 됩니다.
+		switch (id)
+		{
+		case 11: break;
+		case 12: break;
+		}
 		_getch();
 	}
 };
