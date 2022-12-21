0fea6d46dd0e07c5d60e078d050123e9e5573d58
ecourse <smkang@codenuri.co.kr>
2022-12-22 08:59:13 +0900

    Add files via upload

---
+++ DAY4/1_싱글톤2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+/*
+// 싱글톤 코드를 자동생성하는 매크로
+#define MAKE_SINGLETON(classname)				\
+private:										\
+	classname() {}								\
+	classname(const classname&) = delete;		\
+	classname& operator=(const classname&) = delete;	\
+public:													\
+	static classname& getInstance()						\
+	{													\
+		static classname instance;						\
+		return instance;								\
+	}
+*/
+#include "helper.h" // 이 안에 위 매크로 있습니다.
+
+class Cursor
+{
+	MAKE_SINGLETON(Cursor);
+};
+
+int main()
+{
+	Cursor& c1 = Cursor::getInstance();
+	Cursor& c2 = Cursor::getInstance();
+
+	std::cout << &c1 << std::endl;
+	std::cout << &c2 << std::endl;
+
+}
+
+
+
+
+
+
+
+
+
