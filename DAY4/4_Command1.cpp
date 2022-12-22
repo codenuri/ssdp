f2c4fbb58e4bd24fb7ebd0fec0c53d9b5474c83d
ecourse <smkang@codenuri.co.kr>
2022-12-22 14:00:55 +0900

    Add files via upload

--- DAY4/4_Command1.cpp
+++ DAY4/4_Command1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 #include "Helper.h"
 
 class Shape
@@ -20,9 +21,87 @@ public:
 	void draw() override { std::cout << "draw Circle" << std::endl; }
 };
 
+// C 언어 : 프로그램의 기본은 "함수"
+// => 메뉴 선택시 해야할 일을 함수로 작성
+// => "해야할 일" 만 작성한다.
+
+// C++ 언어 : 프로그램의 기본은 "클래스"
+// => 메뉴 선택시 해야할 일을 클래스로 작성
+// => "해야할 일 + 취소동작 + 기타 멤버" 등을 묶어서 관리할수 있다.
+
+//----------------------------------------------------------
+// 모든 명령은 아래 인터페이스를 구현해야 한다.
+struct ICommand
+{
+	virtual void execute() = 0;
+	virtual bool can_undo() { return false; }
+	virtual void undo() {}
+	virtual ~ICommand() {}
+};
+
+// 사각형을 추가하는 메뉴에 대한 명령객체
+class AddRectCommand : public ICommand
+{
+	std::vector<Shape*>& v;
+public:
+	AddRectCommand(std::vector<Shape*>& v) : v(v) {}
+
+	virtual void execute()  override { v.push_back(new Rect); }
+	virtual bool can_undo() override { return true; }
+
+	virtual void undo() override
+	{
+		Shape* p = v.back();
+		v.pop_back();
+
+		delete p;
+	}
+};
+
+class AddCircleCommand : public ICommand
+{
+	std::vector<Shape*>& v;
+public:
+	AddCircleCommand(std::vector<Shape*>& v) : v(v) {}
+
+	virtual void execute()  override { v.push_back(new Circle); }
+	virtual bool can_undo() override { return true; }
+
+	virtual void undo() override
+	{
+		Shape* p = v.back();
+		v.pop_back();
+		delete p;
+	}
+};
+
+class DrawCommand : public ICommand
+{
+	std::vector<Shape*>& v;
+public:
+	DrawCommand(std::vector<Shape*>& v) : v(v) {}
+
+	virtual void execute()  override 
+	{
+		for (auto p : v) p->draw();
+	}
+	virtual bool can_undo() override { return true; }
+
+	virtual void undo() override
+	{
+		system("cls");
+	}
+};
+
+
+
 int main()
 {
 	std::vector<Shape*> v;
+
+	ICommand* pcmd = nullptr;
+
+	std::stack<ICommand*> cmd_stack;
 
 	while (1)
 	{
@@ -31,18 +110,23 @@ int main()
 
 		if (cmd == 1)
 		{
-			v.push_back(new Rect);
+			pcmd = new AddRectCommand(v);
+			pcmd->execute();
+			cmd_stack.push(pcmd);
 		}
 		else if (cmd == 2)
 		{
-			v.push_back(new Circle);
+			pcmd = new AddCircleCommand(v);
+			pcmd->execute();
+			cmd_stack.push(pcmd);
+
 		}
 		else if (cmd == 9)
 		{
-			for (int i = 0; i < v.size(); i++)
-			{
-				v[i]->draw();
-			}
+			pcmd = new DrawCommand(v);
+			pcmd->execute();
+			cmd_stack.push(pcmd);
+
 		}
 	}
 }
