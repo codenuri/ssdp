0ff8c1f99fde50f4dd437d8d5918dfb5cceb39e5
ecourse <smkang@codenuri.co.kr>
2022-12-23 10:43:36 +0900

    Add files via upload

--- DAY5/4_GUI2.cpp
+++ DAY5/4_GUI2.cpp
@@ -10,7 +10,14 @@ public:
 		handle = ec_make_window(&msg_handler, title);
 	}
 
-	int msg_handler(int hwnd, int msg, int a, int b)
+	// 핵심 : 아래 함수를 static 멤버 함수로 해야 하는 이유를 반드시 이해 해야 합니다.
+	// => ec_make_window 는 C언어 기반 함수 인데, 1번째 인자로 인자가 4개인 함수를
+	//    요구 합니다.
+	// => 클래스 멤버 함수로 인자가 "int 4개" 인 함수를 만들려면
+	//    static 멤버 함수 밖에 없습니다
+	// => static 이 아니면 "this" 추가 됩니다.
+
+	static int msg_handler(int hwnd, int msg, int a, int b)
 	{
 		switch (msg)
 		{
