db0d161321d550eba1bb53aacb9302074c39dbd5
ecourse <smkang@codenuri.co.kr>
2024-10-28 11:18:49 +0900

    Add files via upload

--- DAY1/3_가상함수2.cpp
+++ DAY1/3_가상함수2.cpp
@@ -21,6 +21,21 @@ int main()
 
 	// 그런데, 아래 코드가 핵심입니다.
 	// => 실행해서 결과 확인해 보세요
-	Base* p = new Derived;
-	delete p;
+	Base* p = new Derived; // Derived 생성자 호출
+	delete p;				// ?? 소멸자 호출
+
+	// 소멸자 호출도 "함수 호출" 과정입니다.
+	// => static binding 할지 dynamic binding 할지 결정해야 합니다.
+
+	// static binding : 포인터 타입으로 결정
+	// dynamic binding : 실제 메모리에 있는 객체조사후 결정
+
+	// static 으로 할지 dynamic 으로 할지는 
+	// 함수 선언부를 보고 virtual 있는지 조사
+	// 
+	// #1. 컴파일러는 p가 Base*라는 사실만 알수 있다.
+	// #2. 그래서 Base 클래스 선언을 조사해서 소멸자가 virtual 인지조사
+	// #3. Base 소멸자가
+	// virtual 이 아니면 : static binding
+	// virtual 이면      : dynamic binding
 }	
