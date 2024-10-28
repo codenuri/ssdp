4d289280a2f94e889e809f53f0a1c58d9cba86b9
ecourse <smkang@codenuri.co.kr>
2024-10-28 10:29:27 +0900

    Add files via upload

---
+++ DAY1/2_upcasting3.cpp
@@ -0,0 +1,38 @@
+﻿// upcasting3.cpp
+class Animal 
+{
+public: 
+	int age;
+};
+class Cat : public Animal 
+{
+};
+class Dog : public Animal
+{
+public:
+	int color;
+};
+
+// upcasting 활용 #2. 동종을 처리하는 함수 만들기 
+//void NewYear(Dog* pDog) // Dog 객체만 처리하는 함수
+void NewYear(Animal* p)   // 모든 동물을 처리하는 함수
+{						  // 동종을 처리하는 함수
+	++(p->age); 
+	
+}
+int main()
+{
+	Animal a; NewYear(&a);
+	Dog    d; NewYear(&d);
+}
+
+
+
+
+
+
+
+
+
+
+
