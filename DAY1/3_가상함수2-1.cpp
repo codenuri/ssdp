6ee3f4ca2a42c461ec0f58292e239fc39b3a8892
ecourse <smkang@codenuri.co.kr>
2024-10-28 11:45:31 +0900

    Add files via upload

--- DAY1/3_가상함수2-1.cpp
+++ DAY1/3_가상함수2-1.cpp
@@ -2,11 +2,23 @@
 
 #include <iostream>
 
+// 1. 기반 클래스의 소멸자는 기본적으로 "virtual" 이어야 합니다.
+
+// 2. 그런데, virtual 함수가 있으면
+// => 가상함수 테이블이 메모리에 놓이게 됩니다. - 메모리 사용량 증가
+// => 호출시, 메모리 조사때문에 성능저하. 
+
+// 3. 아래 같은 코드가 없다면 소멸자를 "virtual" 로하지 않아도 됩니다.
+// Base* p = new Derived;
+// delete p; // <== 이코드 
+
+// 4. 3번처럼 사용했을때 컴파일에러나게 해 봅시다.
+// => protected 소멸자
 
 class Base
 {
-public:
-	virtual ~Base() {} 
+protected:
+	~Base() {} 
 };
 
 class Derived : public Base
