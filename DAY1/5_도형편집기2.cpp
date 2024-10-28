127f1577a21322e2a59d4357edd9a951198e9efc
ecourse <smkang@codenuri.co.kr>
2024-10-28 14:20:55 +0900

    Add files via upload

---
+++ DAY1/5_도형편집기2.cpp
@@ -0,0 +1,28 @@
+// 15page ~ 
+#include <iostream>
+#include <vector>
+
+class Shape
+{
+	int color;
+public:
+	virtual ~Shape() {}
+};
+
+class Rect : public Shape
+{
+public:
+	void draw() { std::cout << "draw rect\n"; }
+};
+
+class Circle : public Shape
+{
+public:
+	void draw() { std::cout << "draw circle\n"; }
+};
+
+int main()
+{
+	std::vector<Shape*> v; 
+}
+
