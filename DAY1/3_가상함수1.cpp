82e48b32d566de06a8fec37f7cc308c9f41385e6
ecourse <smkang@codenuri.co.kr>
2024-10-28 10:42:37 +0900

    Add files via upload

---
+++ DAY1/3_가상함수1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+class Animal
+{
+public:
+	void cry() { std::cout << "Animal cry" << std::endl; } // #1
+};
+
+class Dog : public Animal
+{
+public:
+	// function override : 기반클래스 함수를 파생 클래스가 다시 만드는 것
+	void cry() { std::cout << "Dog cry" << std::endl; }		// #2
+};
+
+int main()
+{
+	Animal a;	a.cry();
+	Dog d;		d.cry();
+
+	Animal* p = &d; 
+
+	p->cry();
+}
+
+
