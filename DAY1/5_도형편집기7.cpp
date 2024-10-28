0f1f94fd76505e2170069f114f30a5cd9adab31e
ecourse <smkang@codenuri.co.kr>
2024-10-28 15:34:45 +0900

    Add files via upload

--- DAY1/5_도형편집기7.cpp
+++ DAY1/5_도형편집기7.cpp
@@ -8,7 +8,31 @@ class Shape
 public:
 	virtual ~Shape() {}
 
-	virtual void draw() { std::cout << "draw shape\n"; }
+	// 핵심 : 공통성과 가변성의 분리
+	// => 변하지 않은 코드 흐름 속에 있는
+	// => 변해야 하는 코드는 분리되어야 한다.
+	// => 변하는 것을 별도의 가상함수로 분리한다.
+
+	virtual void draw_imp()
+	{
+
+	}
+
+	virtual void draw() 
+	{ 
+		std::cout << "lock mutex\n";
+
+		std::cout << "draw shape\n";
+		
+		std::cout << "unlock mutex\n";
+
+	}
+
+
+
+
+
+
 
 	void set_color(int c) { color = c; }
 
