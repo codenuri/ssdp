5c7134b6d49e85fba5f8af6f9ce3dedb06625e69
ecourse <smkang@codenuri.co.kr>
2024-10-28 14:00:57 +0900

    Add files via upload

---
+++ DAY1/5_도형편집기1.cpp
@@ -0,0 +1,21 @@
+﻿// 15page ~ 
+#include <iostream>
+#include <vector>
+
+// 파워 포인트 같은 프로그램을 만드는 것을 생각해 봅시다.
+
+// #1. 모든 도형을 타입화 하는 것이 편리합니다.
+
+class Rect
+{
+	// int x, y, w, h 와 생성자 등은 생략
+public:
+	void draw() { std::cout << "draw rect\n"; }
+};
+
+
+int main()
+{
+
+}
+
