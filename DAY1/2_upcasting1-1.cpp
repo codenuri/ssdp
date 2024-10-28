4081c17b5a26cdba4f7a38b9dac68ccddba4d648
ecourse <smkang@codenuri.co.kr>
2024-10-28 10:13:34 +0900

    Add files via upload

--- DAY1/2_upcasting1-1.cpp
+++ DAY1/2_upcasting1-1.cpp
@@ -15,12 +15,23 @@ int main()
 	Animal a;
 	Dog    d;
 
-	Animal* p = &d;
+//	Animal* p = &d;
+	Animal* p = &a;
 	
-	// static_cast : 
+//	p->age = 10;   // ok
+//	p->color = 10; // error
+
+	// Animal* 인 p 를 가지고 Dog 고유 멤버에 접근하려면
+	// Dog* 로 캐스팅해야 합니다.
+	 
+	// static_cast : 컴파일 시간 캐스팅.! 실행시 오버헤드 없음
+	//				 컴파일 시간에는 p가 가리키는 곳을 조사할수 없음.
+	//				 실행할때 변경될수 있으므로!!
+	//				 그래서 아래코드는 무조건 성공
 	Dog* dog = static_cast<Dog*>(p);
 
 	std::cout << dog << std::endl;
+
 }
 
 
