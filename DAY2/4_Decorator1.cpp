c3a3eb79b8f78d4065f4e2a85267b545e172ca74
ecourse <smkang@codenuri.co.kr>
2024-10-29 13:53:46 +0900

    Add files via upload

---
+++ DAY2/4_Decorator1.cpp
@@ -0,0 +1,33 @@
+﻿// Decorator - 65 page
+#include <iostream>
+
+// "스티커 사진" 클래스라고 생각해 보세요. 
+class PhotoSticker
+{
+public:
+	void take() { std::cout << "take Photo\n";}
+	void draw()	{ std::cout << "draw Photo\n"; }
+};
+
+// 상속을 사용한 기능의 추가
+class Emoticon : public PhotoSticker
+{
+public:
+	void draw()
+	{
+		std::cout << "@@@@@@@@@@@@@@\n"; // 추가된 기능
+		PhotoSticker::draw();			 // 원래 기능
+		std::cout << "@@@@@@@@@@@@@@\n"; // 추가된 기능
+	}
+};
+
+int main()
+{
+	PhotoSticker ps;
+	ps.take();	// 사진 촬영
+	ps.draw();	// 인쇄
+
+	Emoticon e;
+	e.take();
+	e.draw(); // 사진 + 이모티콘 을 같이 출력
+}
