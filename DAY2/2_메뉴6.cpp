8886b6f137f20c6ed90e5fee658d0f73a9e5e935
ecourse <smkang@codenuri.co.kr>
2024-10-29 11:25:14 +0900

    Add files via upload

--- DAY2/2_메뉴6.cpp
+++ DAY2/2_메뉴6.cpp
@@ -89,20 +89,34 @@ int main()
 	PopupMenu* pm2  = new PopupMenu("해상도 변경");
 
 	root->add(pm1);
-	root->add(pm2);	
+//	root->add(pm2);	
+	pm1->add(pm2);
+
 	root->add(new MenuItem("재부팅", 12));
 
-	pm1->add(new MenuItem("RED", 21));
+	pm1->add(new MenuItem("RED", 21) );
 	pm1->add(new MenuItem("BLUE", 22));
+	pm1->add(new MenuItem("GREEN", 23));
 
 	pm2->add(new MenuItem("HD",  31));
 	pm2->add(new MenuItem("UHD", 32));
 
 	// 이제 시작하려면 ??
-
+	root->command();
 
 }
 
 
+// 객체지향 프로그래밍 세계에서 "프로그램은!!"
 
+// 1. 객체를 생성하고
+// 2. 객체간의 관계를 설정하고
+// 3. 객체간의 메세지를 주고 받는(서로의 멤버함수 호출) 과정이다.
+
+// 4. 프로그램의 기본 단위는 "함수" 가 아닌 "클래스"이다.
+
+// 장점 : 유연성이 좋고, 유지보수가 쉽다.
+
+// 단점 : 메모리 사용량 증가하고
+//		  결정적으로 어렵다!!
 
