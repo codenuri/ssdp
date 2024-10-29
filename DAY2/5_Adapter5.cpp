b19738f94f65be7d4c6eada383d50bb5d68d9546
ecourse <smkang@codenuri.co.kr>
2024-10-29 16:51:10 +0900

    Add files via upload

--- DAY2/5_Adapter5.cpp
+++ DAY2/5_Adapter5.cpp
@@ -24,5 +24,10 @@ int main()
 	// 위 코드는 "상속을 사용한 stack adapter"
 	// 장점 : 가독성이 좋아집니다.(list 가 아닌 stack 사용)
 	// 단점 : 아래 처럼 실수 할수 있습니다.
-	s.push_front(0);
+	s.push_front(0);  // list 로 부터 물려받으므로 사용가능
+					  // 하지만 사용할수 없게 해야 합니다.
+
+	// 상속을 사용한 stack adapter : 나쁜 디자인
+	// => java stack 이 이렇게 되어 있습니다.
+	// => 그래서, java 에서는 java stack  사용하지 말라고 권장(deque 사용)
 }
