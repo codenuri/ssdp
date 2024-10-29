71d471e3a8e0f38f3ae0096821756f629d15fc2c
ecourse <smkang@codenuri.co.kr>
2024-10-29 15:10:09 +0900

    Add files via upload

---
+++ DAY2/4_Decorator8.cpp
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+struct Stream
+{
+	virtual void write(const std::string&) = 0;
+	virtual ~Stream() {}
+
+};
+
+class FileStream : public Stream
+{
+	FILE* file;
+public:
+	FileStream(const char* s, const char* mode = "wt")
+	{
+		file = fopen(s, mode);
+	}
+	~FileStream() { fclose(file); }
+
+	void write(const std::string& s)
+	{
+		printf("%s 쓰기\n", s.c_str());
+	}
+};
+
+class ZipDecoator : public Stream
+{
+	Stream* origin;
+public:
+	ZipDecoator(Stream* s) : origin(s) {}
+
+	void write(const std::string& s)
+	{
+		auto s2 = "[ " + s + "] 압축됨"; // 압축기능 수행
+
+		origin->write(s2);				// 원본 객체에 전달
+	}
+};
+
+class EncryptDecoator : public Stream
+{
+	Stream* origin;
+public:
+	EncryptDecoator(Stream* s) : origin(s) {}
+
+	void write(const std::string& s)
+	{
+		auto s2 = "[ " + s + "] 암호화됨"; 
+
+		origin->write(s2);				
+	}
+};
+
+int main()
+{
+	FileStream fs("a.txt");
+	fs.write("hello");
+
+	EncryptDecoator ed(&fs);
+	ed.write("hello");  // 1. 데이타를 암호화 하고
+	// 2. fs.write(암호화된데이타)
+
+	ZipDecorator zd(&ed);
+	zd.write("hello");	// 1. 압축하고
+	// 2. ed.write(압축된데이타)를 암호화
+	// 3. fs.write(압축암호화된데이타)
+}
