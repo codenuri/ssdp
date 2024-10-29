ce3dab057afd6c2be9ac5fceab728f883fed4538
ecourse <smkang@codenuri.co.kr>
2024-10-29 09:17:47 +0900

    Add files via upload

--- DAY2/1_단위전략3.cpp
+++ DAY2/1_단위전략3.cpp
@@ -6,6 +6,15 @@
 //    분리하자는 것
 
 
+// 장점 
+// => 메모리 할당기(할당정책을 담은 클래스)는 vector 뿐 아니라 
+//    다른 컨테이너(list, set, map 등) 에도 사용가능합니다.
+
+// 단점 
+// => 메모리 할당을 할때 사용하는 함수가 가상함수 입니다.
+// => 가상함수를 약간의 오버헤드가 있습니다.
+
+
 // 메모리 할당/해지를 책임지는 "메모리 할당기(allocator)" 를 만들어 봅시다.
 
 template<typename T> struct IAllocator
@@ -53,7 +62,7 @@ public:
 };
 
 int main()
-{
+{ 
 //	vector<int> v(4);
 	vector<int> v(4, new MallocAllocator<int>() );
 
