5affa5236f50302759312e7ac1c439605e603254
ecourse <smkang@codenuri.co.kr>
2024-10-29 14:02:36 +0900

    Add files via upload

--- DAY2/4_Decorator2.cpp
+++ DAY2/4_Decorator2.cpp
@@ -11,16 +11,25 @@ public:
 
 // 상속 : 객체가 아닌 클래스에 기능을 추가 한것
 //	      실행시간(동적)으로 추가한것이 아닌 소스 작성(정적)시에 추가된것
-class Emoticon : public PhotoSticker
+
+class Emoticon 
 {
+	PhotoSticker* origin; // 핵심 : 포인터 또는 참조 멤버(값타입 아님)
+						  // 의미 : 이미 생성되어 있는 객체를 가리키겠다는 것
 public:
+	Emoticon(PhotoSticker* ps) : origin(ps) {}
+
 	void draw()
 	{
-		std::cout << "@@@@@@@@@@@@@@\n"; 
-		PhotoSticker::draw();			 
+		std::cout << "@@@@@@@@@@@@@@\n"; 	
+		origin->draw();
+
 		std::cout << "@@@@@@@@@@@@@@\n"; 
 	}
 };
+
+
+
 
 int main()
 {
@@ -28,7 +37,9 @@ int main()
 	ps.take();	
 	ps.draw();	
 
-	Emoticon e;
-	e.take();
-	e.draw(); 
+	Emoticon e(&ps); // ps 라는 이미 생성된 객체에 
+					 // 기능을 추가하는 e 객체
+
+	e.draw();	// e가 추가한 기능을 사용하고
+				// 다시 ps 의 기능을 사용
 }
