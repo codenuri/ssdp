9798230a7cf08f7704ab58359f101ea080335a09
ecourse <smkang@codenuri.co.kr>
2024-10-29 10:34:40 +0900

    Add files via upload

--- DAY2/2_메뉴2.cpp
+++ DAY2/2_메뉴2.cpp
@@ -7,6 +7,25 @@
 // => "모든 것은 객체이다."
 // => 프로그램에서 필요한 타입을 먼저 설계한다.
 
+class MenuItem
+{
+	std::string title;
+	int id;
+	// std::string hotkey; // "ctrl+s" 같은 단축키 조합
+public:
+	MenuItem(const std::string& title, int id)
+		: title(title), id(id) {}
+
+	std::string get_title() { return title; }
+
+	// 메뉴 선택시 호출되는 함수
+	void command()
+	{
+		std::cout << get_title() << " 메뉴 선택\n";
+		_getch();
+	}
+};
+
 int main()
 {
 	MenuItem m1("김밥", 11);
