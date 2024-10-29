205b4395f8c28e1a1f0f3a76e43ad625246449a1
ecourse <smkang@codenuri.co.kr>
2024-10-29 09:56:33 +0900

    Add files via upload

---
+++ DAY2/1_단위전략5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::vector<int> v;							// 이 코드는 결국
+//	std::vector<int, std::allocator<int>> v;	// 이 코드 입니다.
+
+
+	v.resize(4);
+}
---
+++ DAY2/1_단위전략6.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+
+// 사용자 정의 메모리 할당기(STL 컨테이너용)
+// 1. 반드시 템플릿일 필요는 없지만 다양한 타입에 사용하기 위해
+//    대부분 템플릿
+// 2. 반드시 약속된 함수를 만들어야 한다.
+template<typename T>
+class debug_alloc
+{
+
+};
+
+
+int main()
+{
+	std::vector<int, debug_alloc<int> > v;
+
+	std::cout << "-----------------" << std::endl;
+
+	v.resize(4);
+
+	std::cout << "-----------------" << std::endl;
+
+	v.resize(8);
+
+	std::cout << "-----------------" << std::endl;
+}
