b38a03090f62daa5d29a7d66247eb3f145e98120
ecourse <smkang@codenuri.co.kr>
2024-10-30 16:08:42 +0900

    Add files via upload

--- DAY3/6_싱글톤2.cpp
+++ DAY3/6_싱글톤2.cpp
@@ -2,6 +2,16 @@
 
 // static 멤버 데이타 vs static 지역변수
 
+// static 지역변수 : 함수가 최초 호출되는 순간 Cursor() 생성자 호출
+//					함수를 호출하지 않으면 Cursor() 생성자 호출 안됨
+//					지연된 초기화
+
+// static 멤버 데이타 : main 함수 가 실행되기 전에 Cursor 생성자 호출
+//						한번도 사용하지 않아도 Cursor 생성자 호출
+//						이른 초기화.
+
+
+
 class Cursor
 {
 private:
@@ -10,20 +20,19 @@ private:
 	Cursor& operator=(const Cursor&) = delete;	
 
 //	Cursor instance;		// error
-//	static Cursor instance; // ok. static 멤버 데이타로 자신 타입가능
+	static Cursor instance; // ok. static 멤버 데이타로 자신 타입가능
 public:
 	static Cursor& get_instance()
 	{
-		static Cursor instance; 
+//		static Cursor instance; 
 		return instance;
 	}
 };
-//Cursor Cursor::instance; // static 멤버 데이타는 외부 선언 필요
-
-
+Cursor Cursor::instance; // static 멤버 데이타는 외부 선언 필요
 
 int main()
 {
+	std::cout << "=======\n";
 //	Cursor& c1 = Cursor::get_instance();
 
 }
