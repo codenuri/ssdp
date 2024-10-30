6f397b65f2c4e2adc459c24b0f94203b28716c87
ecourse <smkang@codenuri.co.kr>
2024-10-30 14:11:56 +0900

    Add files via upload

---
+++ DAY3/3_facade3.cpp
@@ -0,0 +1,91 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <iostream>
+#include <WinSock2.h>
+#pragma comment(lib, "ws2_32.lib")
+using namespace std;
+
+
+class NetworkInit
+{
+public:
+	NetworkInit()
+	{
+
+		WSADATA w;
+		WSAStartup(0x202, &w);
+	}
+	~NetworkInit()
+	{
+
+		WSACleanup();
+	}
+};
+
+class IPAddress
+{
+	SOCKADDR_IN addr;
+public:
+	IPAddress(const char* ip, short port)
+	{
+		addr.sin_family = AF_INET;
+		addr.sin_port = htons(port);
+		addr.sin_addr.s_addr = inet_addr(ip);
+	}
+	SOCKADDR* getRawAddress()
+	{
+		return (SOCKADDR*)&addr;
+	}
+};
+
+class Socket
+{
+	int sock;
+public:
+	Socket(int type) { sock = socket(PF_INET, type, 0); }
+
+	void Bind(IPAddress* ip)
+	{
+		::bind(sock, ip->getRawAddress(), sizeof(SOCKADDR_IN));
+	}
+	void Listen() { ::listen(sock, 5); }
+
+	void Accept()
+	{
+		struct sockaddr_in addr2 = { 0 };
+		int sz = sizeof(addr2);
+
+		accept(sock, (SOCKADDR*)&addr2, &sz);
+	}
+};
+
+// TCP Server 를 만들려면 
+// 1. 몇개의 클래스를 사용해서
+// 2. 일련의 절차를 수행해야 합니다.
+// => 이런 작업도 "하나의 책임(TCPServer라는 책임)" 입니다.
+
+// 이런 과정을 단순화 해서 "하나의 인터페이스(함수)로 제공" 하는 클래스를 만들어라
+// => "facade" 패턴
+
+class TCPServer
+{
+public:
+	void Start(const char* ip, short port)
+	{
+
+	}
+};
+
+
+
+int main()
+{
+	NetworkInit init;
+
+	Socket sock(SOCK_STREAM); // TCP 서버
+
+	IPAddress addr("127.0.0.1", 4000);
+	sock.Bind(&addr);
+	sock.Listen();
+	sock.Accept();
+
+}
