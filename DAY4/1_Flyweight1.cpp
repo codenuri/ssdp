0b06c774dcda93d356b239a45c33c29dd660009a
ecourse <smkang@codenuri.co.kr>
2024-10-31 08:44:19 +0900

    Add files via upload

---
+++ DAY4/1_Flyweight1.cpp
@@ -0,0 +1,31 @@
+﻿// 1_Flyweight - 167
+#include <iostream>
+#include <string>
+#include <map>
+
+
+// 속성이 동일한 객체는  공유 할수 있게 하자.
+// Image 클래스 : 인터넷에서 그림을 다운로드해서 화면에 출력하는 기능
+class Image
+{
+	std::string image_url;
+public:
+	Image(const std::string& url) : image_url(url)
+	{
+		std::cout << url << " Downloading..." << std::endl;
+	}
+	void Draw() { std::cout << "Draw " << image_url << std::endl; }
+};
+
+int main()
+{
+	Image* img1 = new Image("www.naver.com/a.png");
+	img1->Draw();
+
+	Image* img2 = new Image("www.naver.com/a.png");
+	img2->Draw();
+
+}
+
+
+
---
+++ DAY4/1_Flyweight2.cpp
@@ -0,0 +1,39 @@
+﻿// 1_Flyweight - 167
+#include <iostream>
+#include <string>
+#include <map>
+
+
+class Image
+{
+	std::string image_url;
+
+	Image(const std::string& url) : image_url(url)
+	{
+		std::cout << url << " Downloading..." << std::endl;
+	}
+
+public:
+	void Draw() { std::cout << "Draw " << image_url << std::endl; }
+
+
+	// Image 객체를 만드는 static 멤버 함수
+	static Image* Create(const std::string& url)
+	{
+		Image* img = new Image(url);
+		return img;
+	}
+};
+
+int main()
+{
+	Image* img1 = Image::Create("www.naver.com/a.png");
+	img1->Draw();
+
+	Image* img2 = Image::Create("www.naver.com/a.png");
+	img2->Draw();
+
+}
+
+
+
