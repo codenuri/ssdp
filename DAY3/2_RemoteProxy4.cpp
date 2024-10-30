198a692dedd859af6afde28691f163c05a81c3d3
ecourse <smkang@codenuri.co.kr>
2024-10-30 09:40:36 +0900

    Add files via upload

---
+++ DAY3/2_RemoteProxy4.cpp
@@ -0,0 +1,41 @@
+#define USING_GUI
+#include "cppmaster.h"
+
+
+// ICalc.h 만들고 아래 인터페이스 복사 하세요
+struct ICalc
+{
+	virtual int Add(int a, int b) = 0;
+	virtual int Sub(int a, int b) = 0;
+	virtual ~ICalc() {}
+};
+
+class Calc : public ICalc
+{
+	int server;
+public:
+	Calc() { server = ec_find_server("Calc"); }
+
+	int Add(int a, int b) { return ec_send_server(server, 1, a, b); }
+	int Sub(int a, int b) { return ec_send_server(server, 2, a, b); ; }
+};
+
+int main()
+{
+
+	ICalc* calc = new ? ;
+
+
+
+	int n1 = calc->Add(1, 2);
+	int n2 = calc->Sub(1, 2);
+
+	std::cout << n1 << std::endl;
+	std::cout << n2 << std::endl;
+
+}
+
+
+
+
+
---
+++ DAY3/ICalc.h
@@ -0,0 +1,9 @@
+// ICalc.h
+#pragma once
+
+struct ICalc
+{
+	virtual int Add(int a, int b) = 0;
+	virtual int Sub(int a, int b) = 0;
+	virtual ~ICalc() {}
+};
