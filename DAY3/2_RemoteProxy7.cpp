c06f4b2efe74baf77975e71edcea88823131db4f
ecourse <smkang@codenuri.co.kr>
2024-10-30 11:01:11 +0900

    Add files via upload

--- DAY3/2_RemoteProxy7.cpp
+++ DAY3/2_RemoteProxy7.cpp
@@ -19,13 +19,22 @@ public:
 	sp(const sp& other) : obj(other.obj) { if (obj != nullptr) obj->AddRef(); };
 
 	~sp() { if (obj != nullptr) obj->Release(); }
-};
 
+	// sp 를 진짜 포인터 처럼 -> 와 * 를 사용할수 있게 해야 한다.
+	T* operator->() { return obj; }
+	T& operator*() { return *obj; }
+};
 
 int main()
 {
 	sp<ICalc> calc1 = load_proxy(); // sp<ICalc> calc1( load_proxy() )
 	sp<ICalc> calc2 = calc1;
+
+	int n = calc1->Add(10, 20); // calc1은 포인터를 대신하므로
+								// -> 와 * 연산이 되야 합니다.
+								// "연산자 재정의" 문법으로 만들면 됩니다.
+
+	std::cout << n << std::endl;
 }
 
 /*
