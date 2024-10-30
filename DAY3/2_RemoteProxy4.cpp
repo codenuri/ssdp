63e1e74b29b1da6d092c4d67d9a1d77ede93ae2a
ecourse <smkang@codenuri.co.kr>
2024-10-30 10:06:14 +0900

    Add files via upload

--- DAY3/2_RemoteProxy4.cpp
+++ DAY3/2_RemoteProxy4.cpp
@@ -8,22 +8,6 @@
 
 #include "ICalc.h"
 
-ICalc* load_proxy()
-{
-	// #1. DLL 을 load 합니다.
-	void* addr = ec_load_module("CalcProxy.dll");
-				// linux : dlopen()   windows : LoadLibrary()
-
-	// #2. DLL 에서 약속된 함수("create") 를 찾습니다.
-	using FP = ICalc*(*)(); // 함수 포인터 타입
-				// typedef ICalc*(*FP)()
-
-	FP f = (FP)ec_get_function_address(addr, "create");
-				// linux : dlsym()   windows : GetProcAddress()
-
-	// #3. 약속된 함수를 호출해서 Proxy 를 생성해서 반환
-	return f();  // DLL 내부에서 new Calc;
-}
 
 int main()
 {
--- DAY3/ICalc.h
+++ DAY3/ICalc.h
@@ -7,3 +7,26 @@ struct ICalc
 	virtual int Sub(int a, int b) = 0;
 	virtual ~ICalc() {}
 };
+
+
+
+// Proxy Update 시 규칙
+// 1. DLL 의 이름을 변경하면 안됩니다.("CalcProxy.dll 유지")
+// 2. DLL 안에 "create" 가 있어야 합니다.
+
+ICalc* load_proxy()
+{
+	// #1. DLL 을 load 합니다.
+	void* addr = ec_load_module("CalcProxy.dll");
+	// linux : dlopen()   windows : LoadLibrary()
+
+// #2. DLL 에서 약속된 함수("create") 를 찾습니다.
+	using FP = ICalc * (*)(); // 함수 포인터 타입
+	// typedef ICalc*(*FP)()
+
+	FP f = (FP)ec_get_function_address(addr, "create");
+	// linux : dlsym()   windows : GetProcAddress()
+
+// #3. 약속된 함수를 호출해서 Proxy 를 생성해서 반환
+	return f();  // DLL 내부에서 new Calc;
+}
