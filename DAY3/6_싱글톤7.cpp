15f388d4eb7fab6a2439e2eb7aba65737c2a08da
ecourse <smkang@codenuri.co.kr>
2024-10-30 17:09:49 +0900

    Add files via upload

---
+++ DAY3/6_싱글톤7.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+
+class Cursor
+{
+private:
+	Cursor() {}
+
+	Cursor(const Cursor&) = delete;				
+	Cursor& operator=(const Cursor&) = delete;	
+
+public:
+
+	// C++11 부터 static 지역변수의 생성자 호출은
+	// 멀티스레드에 안전합니다.
+	// 아래코드에서 "스레드 A" 가 먼저 도착해서 생성자를 호출하고 있을때
+	// "스레드 B" 가 도착하면 A의 생성자 호출이 종료될때까지 대기 합니다.
+	// => 즉, 동기화 됨.
+	// => 동기화 방법은 "spin lock(busy waiting)" 입니다.
+	static Cursor& get_instance()
+	{
+		static Cursor instance; 
+
+		return instance;
+	}
+
+};
+int main()
+{
+
+
+
+}
+
+
+
+
+
+
+
