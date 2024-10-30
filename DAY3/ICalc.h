10a4b12b3d3d8b59452c7d744e7e148b6b999f19
ecourse <smkang@codenuri.co.kr>
2024-10-30 10:40:43 +0900

    Add files via upload

--- DAY3/ICalc.h
+++ DAY3/ICalc.h
@@ -1,12 +1,36 @@
 // ICalc.h
 #pragma once
 
-struct ICalc
+// Proxy 객체의 수명을 참조 계수(reference counting) 으로
+// 관리한다면
+// 참조 계수 함수들은 인터페이스에 있어야 합니다.
+
+// 다양한 서버를 만들고, 해당 서버의 Proxy 를 만든다고 할때
+// Proxy 에 따라, Add, Sub 이름을 달라지지만
+// 참조계수 함수는 동일합니다.
+// 참조계수 함수는 별도의 인터페이스로!!
+
+struct IRefCount
+{
+	virtual void AddRef() = 0;
+	virtual void Release() = 0;
+
+	virtual ~IRefCount() {}
+};
+
+struct ICalc : public IRefCount
 {
 	virtual int Add(int a, int b) = 0;
 	virtual int Sub(int a, int b) = 0;
 	virtual ~ICalc() {}
 };
+
+/*
+struct IMotorServerProxy : public IRefCount
+{
+	// 모터서버용 Proxy 함수
+};
+*/
 
 
 
