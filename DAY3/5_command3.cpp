2da1a998940ac96aaa22cdd52be9e5a5310cf4ff
ecourse <smkang@codenuri.co.kr>
2024-10-30 12:00:10 +0900

    Add files via upload

---
+++ DAY3/5_command3.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Shape
+{
+public:
+	virtual void draw() = 0;
+	virtual ~Shape() {}
+};
+
+class Rect : public Shape
+{
+public:
+	void draw() override { std::cout << "draw rect" << std::endl; }
+};
+
+class Circle : public Shape
+{
+public:
+	void draw() override { std::cout << "draw circle" << std::endl; }
+};
+//--------------------------------------------------
+struct ICommand
+{
+	virtual void execute() = 0;
+	virtual bool can_undo() { return false; }
+	virtual void undo() {}
+
+	virtual ~ICommand() {}
+};
+
+// 프로그램에서 사용하는 모든 명령(작업)을 클래스로 설계합니다.
+class AddRectCommand : public ICommand
+{
+	std::vector<Shape*>& v;
+public:
+	AddRectCommand(std::vector<Shape*>& v) : v(v) {}
+
+	void execute() override { v.push_back(new Rect);}
+
+	bool can_undo() override { return true; }
+
+	void undo() override
+	{
+		Shape* s = v.back(); // 반환만
+		v.pop_back();		 // 제거
+		delete s;
+	}
+};
+
+class AddCircleCommand : public ICommand
+{
+	std::vector<Shape*>& v;
+public:
+	AddCircleCommand(std::vector<Shape*>& v) : v(v) {}
+
+	void execute() override { v.push_back(new Circle); }
+
+	bool can_undo() override { return true; }
+
+	void undo() override
+	{
+		Shape* s = v.back(); // 반환만
+		v.pop_back();		 // 제거
+		delete s;
+	}
+};
+
+class DrawCommand : public ICommand
+{
+	std::vector<Shape*>& v;
+public:
+	DrawCommand(std::vector<Shape*>& v) : v(v) {}
+
+	void execute() override 
+	{ 
+		for (auto e : v)
+			e->draw();
+	}
+
+	bool can_undo() override { return true; }
+
+	void undo() override
+	{
+		system("cls");
+	}
+};
+
+int main()
+{
+	std::vector<Shape*> v;
+
+	ICommand* command = nullptr;
+
+	while (1)
+	{
+		int cmd;
+		std::cin >> cmd;	
+
+		if (cmd == 1)
+		{
+			command = new AddRectCommand(v);
+			command->execute();
+		}
+		else if (cmd == 2) 
+		{
+			command = new AddCircleCommand(v);
+			command->execute();
+		}
+		else if (cmd == 9)
+		{
+			command = new DrawCommand(v);
+			command->execute();
+		}
+	}
+}
+
+
+
+
+
+
+
