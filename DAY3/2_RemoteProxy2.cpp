5ffd8e5d1e7703d7ce6f64f0e931f5fc8d353f13
ecourse <smkang@codenuri.co.kr>
2024-10-30 09:14:11 +0900

    Add files via upload

--- DAY3/2_RemoteProxy2.cpp
+++ DAY3/2_RemoteProxy2.cpp
@@ -16,13 +16,20 @@ class Calc
 public:
 	Calc() { server = ec_find_server("Calc"); }
 
-	void Add(int a, int b) { return ec_send_server(server, 1, a, b); }
-	void Sub(int a, int b) { return ec_send_server(server, 2, a, b); ; }
+	int Add(int a, int b) { return ec_send_server(server, 1, a, b); }
+	int Sub(int a, int b) { return ec_send_server(server, 2, a, b); ; }
 };
 
 int main()
 {
 	// 이제 클라이언트제작자는 서버를 대신하는 클래스인 "Calc" 사용합니다.
+	// #1. 클라이언트 제작자는 IPC 기술을 몰라도 됩니다.
+	// #2. 서버의 장애 처리도 Proxy 가 하면 됩니다.
+	// #3. 자주 사용되는 요청의 결과를 "캐쉬" 할수도 있습니다.
+	// #4. 1, 2 라는 명령코드 대신, "Add", "Sub" 라는 함수를 사용할수 있습니다.
+
+	// Remote Proxy : 원격지 서버를 대신하는 클래스!!
+
 	Calc* calc = new Calc;
 
 	int n1 = calc->Add(1, 2);
