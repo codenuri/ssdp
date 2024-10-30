2132991ab4ebd568c93725508d16b31d0c022faf
ecourse <smkang@codenuri.co.kr>
2024-10-30 10:07:40 +0900

    Add files via upload

---
+++ DAY3/2_RemoteProxy5.cpp
@@ -0,0 +1,19 @@
+﻿#define USING_GUI
+#include "cppmaster.h"
+#include "ICalc.h"
+
+int main()
+{
+	ICalc* calc = load_proxy();	// DLL 안에서 "new Calc" 하게 됩니다.
+								// 사용후 delete 해야 되지 않을까요 ?
+
+	int n1 = calc->Add(1, 2);
+
+
+	delete calc;	// 이렇게 해도 될까요 ?
+}
+
+
+
+
+
