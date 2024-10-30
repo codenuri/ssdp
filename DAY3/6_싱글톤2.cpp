32d01ad9de698b3f554c1ea88e68666cbba0cea6
ecourse <smkang@codenuri.co.kr>
2024-10-30 16:03:23 +0900

    Add files via upload

--- DAY3/6_싱글톤2.cpp
+++ DAY3/6_싱글톤2.cpp
@@ -5,24 +5,26 @@
 class Cursor
 {
 private:
-	Cursor() {}
+	Cursor() { std::cout << "Cursor()\n"; }
 	Cursor(const Cursor&) = delete;				
 	Cursor& operator=(const Cursor&) = delete;	
 
 //	Cursor instance;		// error
-	static Cursor instance; // ok. static 멤버 데이타로 자신 타입가능
+//	static Cursor instance; // ok. static 멤버 데이타로 자신 타입가능
 public:
 	static Cursor& get_instance()
 	{
-//		static Cursor instance; 
+		static Cursor instance; 
 		return instance;
 	}
 };
-Cursor Cursor::instance; // static 멤버 데이타는 외부 선언 필요
+//Cursor Cursor::instance; // static 멤버 데이타는 외부 선언 필요
+
+
 
 int main()
 {
-	Cursor& c1 = Cursor::get_instance();
+//	Cursor& c1 = Cursor::get_instance();
 
 }
 
