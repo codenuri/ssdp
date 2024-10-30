e820d3d25f3e180dc981fd21571b893e507ee433
ecourse <smkang@codenuri.co.kr>
2024-10-30 14:48:42 +0900

    Add files via upload

--- DAY3/4_Bridge1.cpp
+++ DAY3/4_Bridge1.cpp
@@ -14,6 +14,26 @@ public:
 	void stop() { std::cout << "Stop" << std::endl; }
 };
 
+// 
+// IMP3 를 직접 사용하게 되면
+// => 요구사항이 자주 발생하게되면 IMP3 자체가 수정되어야 합니다.
+// => 인터페이스의 변경은 아주 어려운 작업입니다.
+
+// 사용자들이 IMP3 가 아닌 중간층을 사용하게 합니다.
+class MP3
+{
+	IMP3* impl;
+public:
+	MP3(IMP3* p = nullptr) : impl(p) 
+	{
+		if (impl == nullptr)
+			impl = new IPod;
+	}
+
+};
+
+
+
 class People
 {
 public:
@@ -22,6 +42,9 @@ public:
 	{
 		p->play();
 		p->stop();
+
+		// 요구조건이 생겼다.(1분 미리 듣기)
+		p->play_one_minute();
 	}
 };
 
