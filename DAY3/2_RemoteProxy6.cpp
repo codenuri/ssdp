e92954ae5404006bb24c83746e90d01c907d8ad8
ecourse <smkang@codenuri.co.kr>
2024-10-30 10:43:58 +0900

    Add files via upload

--- DAY3/2_RemoteProxy6.cpp
+++ DAY3/2_RemoteProxy6.cpp
@@ -4,12 +4,23 @@
 
 int main()
 {
-	ICalc* calc = load_proxy();	
+	ICalc* calc1 = load_proxy();	
 
-	int n1 = calc->Add(1, 2);
+	calc1->AddRef(); // 규칙 1. Proxy를 생성한후는 참조계수 증가
 
+	ICalc* calc2 = calc1;
+	calc2->AddRef(); // 규칙 2. Proxy 포인터 복사하면 참조계수 증가
+	
 
-	delete calc;	
+	calc1->Release(); // 규칙 3. 더이상 포인터 사용하지 않으면
+					  //		 참조계수 감소
+
+	std::cout << "-----------\n";
+	
+	calc2->Release();	// 이순간 Proxy 가 delete
+
+	std::cout << "-----------\n";
+	
 }
 
 
