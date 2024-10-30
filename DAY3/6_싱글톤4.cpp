d5acd82ef5c32ba5f7f183eace77a0ccfb0162c8
ecourse <smkang@codenuri.co.kr>
2024-10-30 16:16:09 +0900

    Add files via upload

---
+++ DAY3/6_싱글톤4.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <mutex>
+
+// 멀티스레드 환경에서도 오직 한개만 만들수 있도록 동기화
+
+
+
+class Cursor
+{
+private:
+	Cursor() {}
+	Cursor(const Cursor&) = delete;
+	Cursor& operator=(const Cursor&) = delete;
+
+	static Cursor* sinstance;
+	static std::mutex mtx;
+public:
+	static Cursor& get_instance()
+	{
+		mtx.lock();
+
+		if (sinstance == nullptr)
+			sinstance = new Cursor;
+
+		mtx.unlock();
+
+		return *sinstance;
+	}
+};
+Cursor* Cursor::sinstance = nullptr;
+std::mutex Cursor::mtx;
+
+
+int main()
+{
+	Cursor& c1 = Cursor::get_instance();
+
+}
+
+
+
+
+
+
