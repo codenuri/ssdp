9c86755a10aaf7f9f044f101d99a9769bbb17a85
ecourse <smkang@codenuri.co.kr>
2024-10-31 09:28:07 +0900

    Add files via upload

--- DAY4/1_Flyweight4.cpp
+++ DAY4/1_Flyweight4.cpp
@@ -31,5 +31,14 @@ int main()
 	printf("%p, %p\n", sp1, sp2); // 같은 주소(공유)
 }
 
+// 프로그램에서 읽기만 하는 문자열이 필요하면
+// 문자열 배열이 아닌 문자열 포인터 사용
 
+// C++ 
+// std::string : 문자열 소유(동적할당해서 보관)
+// std::string_view : const char* 와 유사한 역활 (문자열 포인터)
+
+// Rust
+// str 타입 : const char*
+// String  : std::string 과 유사. 
 
