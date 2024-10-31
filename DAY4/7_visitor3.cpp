def9b5000a807eef38f9adc975a2bf5197a2c2f3
ecourse <smkang@codenuri.co.kr>
2024-10-31 16:48:58 +0900

    Add files via upload

--- DAY4/7_visitor3.cpp
+++ DAY4/7_visitor3.cpp
@@ -5,19 +5,30 @@
 
 
 
-class TwiceVisitor
+
+// 방문자 인터페이스
+template<typename T>
+struct IVisitor
+{
+	virtual void visit(T& e) = 0;
+
+	virtual ~IVisitor() {}
+};
+
+template<typename T>
+class TwiceVisitor : public IVisitor<T>
 {
 public:
-	void visit(int& e) { e *= 2; }
+	void visit(T& e) { e *= 2; }
 };
+
 
 template<typename T> class MyList : public std::list<T>
 {
 public:
-
 	using std::list<T>::list;  
 
-	void accept(TwiceVisitor* visitor)
+	void accept(IVisitor<T>* visitor)
 	{
 		for (auto& e : *this)  
 			visitor->visit(e);
@@ -28,8 +39,16 @@ int main()
 {
 	MyList<int> s = { 1,2,3,4,5,6,7,8,9,10 };
 
-	TwiceVisitor tv;
+	TwiceVisitor<int> tv;
 	s.accept(&tv);
+
+	ShowVisitor<int> sv; // 모든 요소를 화면 출력하는 방문자
+	s.accept(&sv);
+
+	ZeroVisitor<int> zv; // 모든 요소를 0으로 하는 방문자
+	s.accept(&zv);
+
+	s.accept(&sv);
 }
 
 
