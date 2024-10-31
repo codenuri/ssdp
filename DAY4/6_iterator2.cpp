6c100645956066e9ce88415cb682b37492b49313
ecourse <smkang@codenuri.co.kr>
2024-10-31 14:32:14 +0900

    Add files via upload

---
+++ DAY4/6_iterator2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+// 아래 코드는 "싱글 링크드 리스트" 입니다.
+
+template<typename T> struct Node
+{
+	T     data;
+	Node* next;
+
+	Node(const T& d, Node* n) : data(d), next(n) {}
+};
+
+template<typename T> struct slist
+{
+	Node<T>* head = 0;
+public:
+	void push_front(const T& a) { head = new Node<T>(a, head); }
+};
+
+int main()
+{
+	slist<int> s;
+	s.push_front(10);
+	s.push_front(20);
+	s.push_front(30);
+	s.push_front(40);
+}
