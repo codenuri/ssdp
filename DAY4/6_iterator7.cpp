2ebbd0fccb59925e3c43327e8776e0f89d4b1e0b
ecourse <smkang@codenuri.co.kr>
2024-10-31 15:45:05 +0900

    Add files via upload

--- DAY4/6_iterator7.cpp
+++ DAY4/6_iterator7.cpp
@@ -6,10 +6,23 @@ int main()
 {
 	std::vector<int> v = { 1,2,3,4,5,6,7,8,9,10 };
 
+	// python : for e     in v
+	// C#     : for var e in v
 	for (auto e : v)
 	{
 		std::cout << e << ", ";
 	}
-
 	std::cout << "\n";
+
+	// 위 for 문의 원리
+	// 위 코드를 컴파일러는 아래 처럼 변경
+	auto first = v.begin();
+	auto last = v.end();
+
+	for (; first != last; ++first)
+	{
+		auto e = *first;
+		//----------------------------
+		std::cout << e << ", ";	// 사용자 코드
+	}
 }
