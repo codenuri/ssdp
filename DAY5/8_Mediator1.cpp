cd97c28f14398c412bfb2a57b93c27026d0b3231
ecourse <smkang@codenuri.co.kr>
2024-11-01 14:11:03 +0900

    Add files via upload

---
+++ DAY5/8_Mediator1.cpp
@@ -0,0 +1,37 @@
+﻿// 6_Mediator - 중재자 패턴 
+#include <iostream>
+
+class CheckBox
+{
+	bool state;
+public:
+	CheckBox() : state(false) {}
+	
+	void SetCheck(bool b) { state = b; ChangeState(); }
+	bool GetCheck()       { return state; }
+
+	virtual void ChangeState() {}
+};
+
+
+class MyCheckBox : public CheckBox
+{
+public:
+	void ChangeState() override
+	{
+		if (GetCheck() == true)
+			std::cout << "enable button\n";
+		else 
+			std::cout << "disable button\n";
+	}
+};
+
+
+int main()
+{
+	MyCheckBox c;
+	c.SetCheck(true);
+}
+
+
+
