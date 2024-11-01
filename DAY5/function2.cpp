9c3a3aac37fb08701baebf9d6771348b19cb3fe1
ecourse <smkang@codenuri.co.kr>
2024-11-01 14:48:59 +0900

    Add files via upload

--- DAY5/function2.cpp
+++ DAY5/function2.cpp
@@ -15,8 +15,15 @@ void foo2(int a, int b) {}
 int main()
 {
 	// C언어의 함수 포인터는 유연성이 부족합니다.
-	void (*f1)(int) = &foo1; // ok
-	f1 = &foo2; // ??
+//	void (*f1)(int) = &foo1; // ok
+//	f1 = &foo2; // error. foo2 는 인자가 2개..
 
+	// C++ std::function 은 "함수포인터" 를 대체해서 사용..
+	// 사용법 : std::function<함수모양> 
+
+	std::function<void(int)> f1;
+
+	f1 = &foo1;
+	f1(10); // foo1(10)
 }
 
