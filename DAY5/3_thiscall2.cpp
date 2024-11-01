d90f4ccafb938862318f1417983b5127f9f8eaeb
ecourse <smkang@codenuri.co.kr>
2024-11-01 09:17:13 +0900

    Add files via upload

---
+++ DAY5/3_thiscall2.cpp
@@ -0,0 +1,20 @@
+class Dialog
+{
+public:
+	void close(int code) {}
+
+	static void close2(int code) {}
+};
+
+void foo(int a) {}
+
+int main()
+{
+	void(*f1)(int) = &foo;			 // ok 	
+	void(*f2)(int) = &Dialog::close; // error. 멤버 함수이므로 this 추가된 함수
+
+	void(*f3)(int) = &Dialog::close2; // ok. static 멤버 함수는 this가 추가안됨.
+
+	// 1. 일반함수 포인터에 멤버 함수의 주소를 담을수 없습니다.
+	// 2. 일반함수 포인터에 static 멤버 함수의 주소를 담을수 있습니다.
+}
