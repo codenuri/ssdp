3d75bde41e57d41d0a9256dba6b416a74c33fd28
ecourse <smkang@codenuri.co.kr>
2024-11-01 10:56:51 +0900

    Add files via upload

--- DAY5/5_GUI8.cpp
+++ DAY5/5_GUI8.cpp
@@ -2,11 +2,18 @@
 #include "cppmaster.h"
 #include <map>
 
+
+// event 처리를 가상함수로 하면
+// => 가상함수 테이블의 오버헤드가 있습니다.
+// => 200개 가상함수 * 함수주소크기(8바이트) => 1600 바이트.
+
+
 class Window;
 
 std::map<int, Window*> this_map;  
 
 
+template<typename T>
 class Window
 {
 	int handle;
@@ -21,12 +28,16 @@ public:
 
 	static int msgproc(int hwnd, int msg, int a, int b)
 	{
-		Window* self = this_map[hwnd];
+//		Window* self = this_map[hwnd];
+		T* self = static_cast<T*>(this_map[hwnd]);
 
 		switch (msg)
 		{
 		case WM_LBUTTONDOWN:
 			self->lbutton_down();
+					// 함수 호출
+					// 가상함수 : dynamic binding (실행시간에 메모리조사)
+					// 비가상   : static binding(포인터 타입)
 			break;
 
 		case WM_KEYDOWN:
@@ -36,14 +47,14 @@ public:
 		return 0;
 	}
 
-	virtual void lbutton_down() {}
-	virtual void key_down() {}
+	void lbutton_down() {}
+	void key_down() {}
 };
 
-class MainWindow : public Window
+class MainWindow : public Window< MainWindow>
 {
 public:
-	void lbutton_down() override 
+	void lbutton_down() 
 	{ 
 		std::cout << "MainWindow lbutton_down\n"; 
 	}
