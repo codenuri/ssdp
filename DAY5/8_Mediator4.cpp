785d6f872c86dc8c06fb1f2349815f1ef28d8a11
ecourse <smkang@codenuri.co.kr>
2024-11-01 15:04:22 +0900

    Add files via upload

---
+++ DAY5/8_Mediator4.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <functional>
+#include <vector>
+using namespace std::placeholders;
+
+
+class NotificationCenter
+{
+	using HANDLER = std::function<void(void*)>;
+
+	std::map<std::string, std::vector<HANDLER> > notif_map;
+public:
+
+	void addObserver(const std::string& key, HANDLER h)
+	{
+		notif_map[key].push_back(h);
+	}
+	void postNotificationWithName(const std::string& key, void* hint)
+	{
+		for (auto f : notif_map[key]) // notif_map[key] 는 vector
+		{
+			f(hint);
+		}
+	}
+};
+
+void foo(void* p)		 { std::cout << "foo : " << (int)p << std::endl; }
+void goo(void* p, int a) { std::cout << "goo : " << (int)p << std::endl; }
+
+
+int main()
+{
+	// IOS 라이브러리에 있는 통보 센터
+	NotificationCenter nc;
+
+	nc.addObserver("LOWBATTERY", &foo);
+	nc.addObserver("LOWBATTERY", &goo);
+	nc.addObserver("DISCONNECT_WIFI", &goo);
+
+	// 배터리 모듈쪽에서 배터리가 부족해지면
+	nc.postNotificationWithName("LOWBATTERY", (void*)30);
+}
+
+
+
+
