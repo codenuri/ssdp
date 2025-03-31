e6e09983f5d8abd6f9d89df969f796b212ed0fcd
ecourse <smkang@codenuri.co.kr>
2025-03-31 09:11:38 +0900

    Add files via upload

--- DAY1/1_생성자3.cpp
+++ DAY1/1_생성자3.cpp
@@ -11,6 +11,13 @@ public:
 	//     멤버를 초기화 하는 것이 좋습니다.
 	Person(const std::string& n, int a) : name(n), age(a) {}
 };
+// 아래 주석대로 만들어 보세요
+// 1. Person 에서 파생된 Student 만들어 보세요
+// 2. 멤버 데이타 "int id" 추가하세요
+// 3. Student 생성자 만들어 보세요
+// 4. main 에서 Student 객체 생성해 보세요
+
+
 
 int main()
 {
