1366a156b46328f76c18376a67532bc7d51cb95d
ecourse <smkang@codenuri.co.kr>
2025-03-31 10:09:01 +0900

    Add files via upload

--- DAY1/2_upcasting1-1.cpp
+++ DAY1/2_upcasting1-1.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 class Animal
 {
 public:
@@ -18,11 +20,21 @@ int main()
 //	Animal* p = &d;	
 	Animal* p = &a;
 
-	// static_cast vs dynamic_cast
+	std::cout << p << std::endl;
 
+	// static_cast vs dynamic_cast
+	// static_cast : 컴파일 시간 캐스팅
+	//				 컴파일러는 p가 가리키는 객체를 조사할수 없다.
+	//				 따라서 아래코드는 항상 성공
+	//				 반환된 p1으로 color 접근은 잘못된 코드
 	Dog* p1 = static_cast<Dog*>(p);
 
+	std::cout << p1 << std::endl;
 
+//	p1->color = 10; // 잘못된 코드.절대 사용하면 안됩니다
+
+	
+	// dynamic_cast : 실행 시간 캐스팅
 }
 
 
