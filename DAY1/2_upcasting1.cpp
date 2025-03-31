0feb608327007c75558a767286cf5bb1f54a43ec
ecourse <smkang@codenuri.co.kr>
2025-03-31 09:46:13 +0900

    Add files via upload

---
+++ DAY1/2_upcasting1.cpp
@@ -0,0 +1,36 @@
+﻿class Animal
+{
+public:
+	int age;
+};
+
+class Dog : public Animal
+{
+public:
+	int color;
+};
+
+int main()
+{
+	Dog d;
+	Dog* p1 = &d;		// ok
+
+//	int* p2 = &d;		// error
+	
+	// 핵심 #1. 기반 클래스타입의 포인터로 파생 클래스 객체를 가리킬수 있다.
+	// => upcasting 이라고 합니다.
+	// => 가능한 이유는 메모리그림을 생각해 보세요
+	Animal* p3 = &d;	// ok.
+
+}
+
+
+
+
+
+
+
+
+
+
+
