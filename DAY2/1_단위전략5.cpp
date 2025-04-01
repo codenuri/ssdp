35a4a30defd75c9674270dc33b0e0295dbb3fb0a
ecourse <smkang@codenuri.co.kr>
2025-04-01 09:19:46 +0900

    Add files via upload

---
+++ DAY2/1_단위전략5.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+//	std::vector<int> v; // std::vector<int, std::allocator<T>> v;
+
+	std::vector<int, debug_alloc<int> > v;
+
+	std::cout << "-----------------" << std::endl;
+
+	v.resize(5);
+
+	std::cout << "-----------------" << std::endl;
+}
