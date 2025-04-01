08ffb81f3e20d9aac40f7f184828cc1535bdc146
ecourse <smkang@codenuri.co.kr>
2025-04-01 17:01:43 +0900

    Add files via upload

---
+++ DAY2/6_Adapter4.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <list>
+#include <vector>
+#include <deque>
+
+// STL 과 Adapter
+// C++ 표준에 list가 이미 있는데, 사용자가 stack 을 요구 합니다.
+// (stack 도 있지만 없다고 가정)
+
+template<typename T> class stack : public std::list<T>
+{
+public:
+	void push(const T& e)	{ std::list<T>::push_back(e); }
+	void pop()				{ std::list<T>::pop_back(); }
+	T& top()				{ return std::list<T>::back(); };
+}
+int main()
+{
+	stack<int> s;
+	s.push(10);
+}
