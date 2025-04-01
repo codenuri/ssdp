ff38d964972513c387402d21fac85e4fab3dff10
ecourse <smkang@codenuri.co.kr>
2025-04-01 17:03:53 +0900

    Add files via upload

--- DAY2/6_Adapter4.cpp
+++ DAY2/6_Adapter4.cpp
@@ -7,7 +7,8 @@
 // C++ 표준에 list가 이미 있는데, 사용자가 stack 을 요구 합니다.
 // (stack 도 있지만 없다고 가정)
 
-template<typename T> class stack : public std::list<T>
+template<typename T> 
+class stack : public std::list<T>
 {
 public:
 	void push(const T& e)	{ std::list<T>::push_back(e); }
@@ -18,4 +19,7 @@ int main()
 {
 	stack<int> s;
 	s.push(10);
+
+	// 위 설계의 단점. 아래 코드처럼 사용한다면 ???
+	s.push_front(20);
 }
