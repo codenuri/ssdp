bd3b28997da7c0f9f5c2c4d2be669252a988cc08
ecourse <smkang@codenuri.co.kr>
2025-04-01 13:01:03 +0900

    Add files via upload

---
+++ DAY2/3_Composite.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+// 탐색기를 객체지향 프로그램으로 만들어 봅시다.
+
+class Component
+{
+	std::string name;
+public:
+	Component(const std::string& name) : name(name) {}
+
+	virtual ~Component() {}
+
+	// File, Folder 모두 크기는 구할수 있다.
+	virtual int get_size() = 0;
+};
+
+
+class File  
+{
+public:
+};
+class Folder 
+{
+public:
+};
+
+
+
+int main()
+{
+	Folder* root = new Folder("ROOT");
+	Folder* fo1  = new Folder("A");
+	Folder* fo2  = new Folder("B");
+	
+	root->add(fo1);
+	root->add(fo2);
+
+	File* f1 = new File("a.txt", 10);
+	File* f2 = new File("b.txt", 20);
+
+	fo1->add(f1);
+	root->add(f2);
+
+	// 파일은 자신만의 크기는 있습니다.
+	// 폴더는 자신만의 크기는 없지만 크기를 구할수 있습니다.
+	std::cout << f2->get_size() << std::endl; // 20
+	std::cout << fo1->get_size() << std::endl; // 10
+	std::cout << root->get_size() << std::endl; // 30
+}
+
