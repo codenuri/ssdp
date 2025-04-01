5d54e6ae63dfde76d2de5bbb819288faf0f70db3
ecourse <smkang@codenuri.co.kr>
2025-04-01 16:53:53 +0900

    Add files via upload

---
+++ DAY2/6_Adapter3.cpp
@@ -0,0 +1,33 @@
+﻿// 디자인 패턴을 열심히 공부하면, 다양한 패턴이 동일(유사)해 보일때가 있습니다.
+// 이럴때는 의도를 생각하세요. 의도가 가장 중요 합니다.
+
+class ObjectAdapter : public Shape
+{
+	TextView* origin;	
+public:
+	ObjectAdapter(TextView* p) : origin(p) {}
+
+	void draw() override { origin->show(); }
+};
+
+class ZipDecorator : public Stream
+{
+	Stream* origin; 
+public:
+	ZipDecorator(Stream* s) : origin(s) {}
+
+	void write(const std::string& s) override
+	{
+		auto s2 = "[( " + s + " ) 압축됨]";
+		origin->write(s2);
+	}
+};
+
+
+TextView tv("hello");
+ObjectAdapter oa(&tv);
+
+
+
+FileStream fs("a.txt");
+ZipDecorator zd(&fs);
