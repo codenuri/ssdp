a49a5a0edd87623a4f88c731b99ee0f4d7eb1f11
ecourse <smkang@codenuri.co.kr>
2025-04-02 11:03:53 +0900

    Add files via upload

---
+++ DAY3/2_RemoteProxy7.cpp
@@ -0,0 +1,50 @@
+﻿#define USING_GUI
+#include "cppmaster.h"
+#include "ICalc.h"
+
+// raw pointer 를 대신 하는 클래스를 만들어 봅시다.
+// => smart proxy 라는 기술
+// => 흔히 "smart pointer" 라고 알려진 기술
+
+template<typename T> 
+class sp
+{
+	T* obj;
+public:
+	sp(T* p = nullptr) : obj(p) {} 
+	sp(const sp& other) : obj(other.obj) {}
+
+	~sp() {}
+};
+
+int main()
+{
+	sp<ICalc> calc1 = load_proxy();
+	sp<ICalc> calc2 = calc1;
+}
+
+
+
+
+
+
+
+int main()
+{
+	ICalc* calc1 = load_proxy();
+	calc1->AddRef();	
+
+	ICalc* calc2 = calc1;
+	calc2->AddRef();	
+
+	calc2->Release();	
+	std::cout << "----------------------\n";
+
+	calc1->Release();	
+
+	std::cout << "----------------------\n";
+
+
+}
+
+
