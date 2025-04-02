b4f1fc9a9bc4d18904739fb652292f6132a25b30
ecourse <smkang@codenuri.co.kr>
2025-04-02 10:17:16 +0900

    Add files via upload

--- DAY3/ICalc.h
+++ DAY3/ICalc.h
@@ -7,3 +7,25 @@ struct ICalc
 	virtual int Sub(int, int) = 0;
 	virtual ~ICalc() {}
 };
+
+
+
+ICalc* load_proxy()
+{
+	// #1. dll load
+	void* addr = ec_load_module("CalcProxy.dll");
+	// windows : LoadLibrary()
+	// linux   : dlopen()
+
+// #2. 약속된 함수 찾기
+	typedef ICalc* (*F)(); // 함수 포인터 타입
+
+	F f = (F)ec_get_function_address(addr, "create");
+	// windows : GetProcAddress()
+	// linux   : dlsym()
+
+//	printf("%p, %p\n", addr, f);
+
+	// #3. 약속된 함수를 호출해서 proxy 생성후 반환
+	return f(); // DLL 내부의 create() 호출
+}
