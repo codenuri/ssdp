3c20014d471ce4ee4ddc8ae1aaf381bf51bff735
ecourse <smkang@codenuri.co.kr>
2025-04-02 16:35:32 +0900

    Add files via upload

---
+++ DAY3/6_Flyweight.cpp
@@ -0,0 +1,31 @@
+﻿// 5_Flyweight - 167
+#include <iostream>
+#include <string>
+#include <map>
+
+
+// 속성이 동일한 객체는  공유 할수 있게 하자.
+
+class Image
+{
+	std::string image_url;
+public:
+	Image(const std::string& url) : image_url(url)
+	{
+		std::cout << url << " Downloading..." << std::endl;
+	}
+	void draw() { std::cout << "Draw " << image_url << std::endl; }
+};
+
+int main()
+{
+	Image* img1 = new Image("www.naver.com/a.png");
+	img1->draw();
+
+	Image* img2 = new Image("www.naver.com/a.png");
+	img2->draw();
+
+}
+
+
+
---
+++ DAY3/6_Flyweight2.cpp
@@ -0,0 +1,37 @@
+﻿// 5_Flyweight - 167
+#include <iostream>
+#include <string>
+#include <map>
+
+class Image
+{
+	std::string image_url;
+
+	Image(const std::string& url) : image_url(url)
+	{
+		std::cout << url << " Downloading..." << std::endl;
+	}
+
+public:
+	void draw() { std::cout << "Draw " << image_url << std::endl; }
+
+	// 자신의 객체를 만드는 static 멤버 함수
+	static Image* create(const std::string& url)
+	{
+		Image* img = new Image(url);
+		return img;
+	}
+};
+
+int main()
+{
+	Image* img1 = Image::create("www.naver.com/a.png");
+	img1->draw();
+
+	Image* img2 = Image::create("www.naver.com/a.png");
+	img2->draw();
+
+}
+
+
+
