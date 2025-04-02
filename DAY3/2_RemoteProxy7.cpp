f94e3599b3773c84d181c31b3d7ac57fa4b795b5
ecourse <smkang@codenuri.co.kr>
2025-04-02 11:12:06 +0900

    Add files via upload

--- DAY3/2_RemoteProxy7.cpp
+++ DAY3/2_RemoteProxy7.cpp
@@ -12,19 +12,25 @@ class sp
 	T* obj;
 public:
 	sp(T* p = nullptr) : obj(p) { if (obj != nullptr) obj->AddRef(); }
-
 	sp(const sp& other) : obj(other.obj) { if (obj != nullptr) obj->AddRef(); }
-
 	~sp() { if (obj != nullptr) obj->Release(); }
+
+	// 스마트 포인터는 "포인터를 대신" 해서 사용하므로
+	// -> 와 * 를 되도록하기 위해서 연산자 재정의를 하게 됩니다.
+	T* operator->() { return obj; }
+	T& operator*() { return *obj; }
 };
 
 int main()
 {
 	sp<ICalc> calc1 = load_proxy(); // sp<ICalc> calc1( load_proxy() )
 	sp<ICalc> calc2 = calc1;
+
+	// calc1 은 raw pointer 대신 사용하는 것이므로
+	// -> 와 * 연산을 지원해야 합니다.
+	int n1 = calc1->Add(10, 20);
+	int n2 = (*calc1).Sub(10, 20);
 }
-
-
 
 
 /*
