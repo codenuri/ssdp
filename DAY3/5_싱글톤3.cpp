a6c926bd58398ebee6da2984b5a8e86d59da8b88
ecourse <smkang@codenuri.co.kr>
2025-04-02 14:47:57 +0900

    Add files via upload

---
+++ DAY3/5_싱글톤3.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+// 오직 한개의 객체를 힙에 만드는 모델
+
+class Cursor
+{
+private:
+	Cursor() {}
+	Cursor(const Cursor&) = delete;
+	Cursor& operator=(const Cursor&) = delete;
+
+	static Cursor* sinstance;
+public:
+	static Cursor& get_instance()
+	{
+		if (sinstance == nullptr)
+			sinstance = new Cursor;
+
+		return *sinstance;
+	}
+};
+Cursor* Cursor::sinstance = nullptr;
+
+
+int main()
+{
+	Cursor& c1 = Cursor::get_instance();
+
+}
+
+
+
+
+
+
