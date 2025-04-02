28bc4c690a7545a73dc721ffb94761ce05ae478c
ecourse <smkang@codenuri.co.kr>
2025-04-02 15:18:25 +0900

    Add files via upload

--- DAY3/5_싱글톤6.cpp
+++ DAY3/5_싱글톤6.cpp
@@ -2,13 +2,16 @@
 #include <mutex>
 
 // CRTP
+// => Curiously Recurring Template Pattern
 // => 기반 클래스에서 "파생 클래스의 클래스이름" 을 사용할수 있게 하는 기술
+// => 기반 클래스의 템플릿 인자로 파생 클래스 이름을 전달하는 기술
 
 template<typename T>
 class Singleton
 {
-private:
+protected:
 	Singleton() {}
+
 	Singleton(const Singleton&) = delete;
 	Singleton& operator=(const Singleton&) = delete;
 
@@ -26,8 +29,8 @@ public:
 		return *sinstance;
 	}
 };
-T* Singleton::sinstance = nullptr;
-std::mutex Singleton::mtx;
+template<typename T> T* Singleton<T>::sinstance = nullptr;
+template<typename T> std::mutex Singleton<T>::mtx;
 
 
 // Mouse 클래스도 위와 같은 싱글톤으로 하고 싶다.
