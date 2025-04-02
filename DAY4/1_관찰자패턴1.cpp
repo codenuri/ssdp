5cf8ded825b415bcdef5ae98fef0c4ea70e95363
ecourse <smkang@codenuri.co.kr>
2025-04-03 08:41:08 +0900

    Add files via upload

---
+++ DAY4/1_관찰자패턴1.cpp
@@ -0,0 +1,80 @@
+﻿//1_관찰자패턴1 - 97 page
+#include <iostream>
+#include <vector>
+
+struct IGraph
+{
+	virtual void update(int data) = 0;
+	virtual ~IGraph() {}
+};
+
+class Table
+{
+	std::vector<IGraph*> v;
+	int value; // table 의 data 값
+public:
+	void attach(IGraph* p) { v.push_back(p); }
+	void detach(IGraph* p) {}
+	void notify(int data)
+	{
+		// 등록된 모든 그래프에 알려준다.
+		for (auto p : v)
+			p->update(data);
+	}
+	void edit()
+	{
+		while (1)
+		{
+			std::cout << "Data >>";
+			std::cin >> value;
+			notify(value);
+		}
+	}
+};
+//----------------------
+class BarGraph : public IGraph
+{
+public:
+	void update(int n) override
+	{
+		std::cout << "Bar Graph : ";
+
+		for (int i = 0; i < n; i++)
+			std::cout << "*";
+
+		std::cout << std::endl;
+	}
+};
+
+class PieGraph : public IGraph
+{
+public:
+	void update(int n) override
+	{
+		std::cout << "Pie Graph : ";
+
+		for (int i = 0; i < n; i++)
+			std::cout << ")";
+
+		std::cout << std::endl;
+	}
+};
+
+int main()
+{
+	Table t;
+
+	BarGraph bg;
+	PieGraph pg;
+
+	t.attach(&bg);
+	t.attach(&pg);
+
+	t.edit(); // 편집 모드
+}
+
+
+
+
+
+
