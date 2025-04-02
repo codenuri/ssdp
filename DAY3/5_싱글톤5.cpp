187e9d295113f2fa10b02326fa5798c930332ad4
ecourse <smkang@codenuri.co.kr>
2025-04-02 15:07:25 +0900

    Add files via upload

--- DAY3/5_싱글톤5.cpp
+++ DAY3/5_싱글톤5.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <mutex>
 
-// RAII
+// RAII IDioms
 // 자원의 획득과 반납은 "생성자/소멸자" 를 통해서 자동으로 되도록 해야 한다!!
+
+// => Resource Acquision Is Initialization
+// => 자원이 할당되는 것은 (자원관리객체의) 초기화 과정이다.
+//    라는 의미...
+
+
+
 template<typename T>
 class lock_guard
 {
@@ -29,12 +36,22 @@ public:
 
 	static Cursor& get_instance()
 	{
-		mtx.lock();
+	
+		std::lock_guard<std::mutex> g(mtx);  // C++ 표준에 이미 있습니다.
+
+//		lock_guard<std::mutex> g(mtx);
+				// #1. g의 생성자에서 lock
+				// #2. g파괴시 소멸자에서 unlock
+				// #3. 함수 중간에 return 문이 있거나 예외가 나와도
+				//	   g는 안전하게 파괴 된다 - 소멸자 호출되고 unlock
+				// #4. 함수 중간에서 unlock 하려면 {} 를 사용하세요
+				
+//		mtx.lock();
 
 		if (sinstance == nullptr)
 			sinstance = new Cursor;
-
-		mtx.unlock();
+		
+//		mtx.unlock();
 
 		return *sinstance;
 	}
