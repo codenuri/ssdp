9d82ed134a683e8f88e0b32388f2b1cd3ecbe4ed
ecourse <smkang@codenuri.co.kr>
2025-04-02 14:51:53 +0900

    Add files via upload

---
+++ DAY3/5_싱글톤4.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <mutex>
+
+// 동기화 버전
+
+class Cursor
+{
+private:
+	Cursor() {}
+	Cursor(const Cursor&) = delete;
+	Cursor& operator=(const Cursor&) = delete;
+
+	
+	static Cursor* sinstance;
+	static std::mutex mtx;
+public:
+	static Cursor& get_instance()
+	{
+		mtx.lock();
+
+		if (sinstance == nullptr)
+			sinstance = new Cursor;
+
+		mtx.unlock();
+		return *sinstance;
+	}
+};
+Cursor* Cursor::sinstance = nullptr;
+std::mutex Cursor::mtx;
+
+
+
+
+int main()
+{
+	Cursor& c1 = Cursor::get_instance();
+
+}
+
+
+
+
+
+
