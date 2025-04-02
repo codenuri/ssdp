b42f4251f59b68071460f4b17981cfb184432d36
ecourse <smkang@codenuri.co.kr>
2025-04-02 09:59:56 +0900

    Add files via upload

--- DAY3/CalcProxy.cpp
+++ DAY3/CalcProxy.cpp
@@ -12,3 +12,25 @@ public:
 	int Add(int a, int b) { return ec_send_server(server, 1, a, b); }
 	int Sub(int a, int b) { return ec_send_server(server, 2, a, b); }
 };
+
+// client 제작자는 
+// 이 DLL 에 있는 proxy class 이름을
+// 알수도없고, 사용해도 안됩니다. => 약한 결합을 위해서
+
+// DLL 내부에 약속된 함수를 만들고 proxy 객체를 생성합니다.
+// => 새로운 proxy dll 을 만들어도 아래 함수는 반드시 제공되어야 합니다.
+
+extern "C" __declspec(dllexport)  // dll 에서 함수 만들때 필요
+ICalc* create()
+{
+	return new Calc;
+}
+
+// 새로운 DLL 이 나와도 이름 한개를 약속한다면..
+// 그냥 proxy class 이름을 "Calc" 로 약속하면 안되나요 ??
+// => new Calc 하려면 "클래스 헤더" 가 필요하고, 클라이언트 자체를 다시 빌드
+//    해야 합니다.
+
+// 함수 이름은 "이름"으로 함수를 찾을수 있습니다.!!
+// => 다시 빌드 필요 없습니다.
+//----------------------------------------
