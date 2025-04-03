c72b3d8f336a1cc0d6ab66f5657c726a092c767f
ecourse <smkang@codenuri.co.kr>
2025-04-03 13:53:23 +0900

    Add files via upload

---
+++ DAY4/6_iterator4.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+template<typename T> struct Node
+{
+	T     data;
+	Node* next;
+
+	Node(const T& d, Node* n) : data(d), next(n) {}
+};
+
+template<typename T> struct slist
+{
+	Node<T>* head = 0;
+public:
+	void push_front(const T& a) { head = new Node<T>(a, head); }
+};
+
+
+int main()
+{
+	slist<int> s;
+	s.push_front(10);
+	s.push_front(20);
+	s.push_front(30);
+	s.push_front(40);
+
+	// 목표 : java 스타일의 반복자
+
+	auto it = s.iterator();
+
+	while (it->hasNext())
+	{
+		std::cout << it->next() << std::endl;
+	}
+}
