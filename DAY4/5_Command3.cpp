e715f46acdd3fcff0757ec843df74f3bb473d2f9
ecourse <smkang@codenuri.co.kr>
2025-04-03 16:58:05 +0900

    Add files via upload

---
+++ DAY4/5_Command3.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+class Shape
+{
+public:
+	virtual void draw() = 0;
+	virtual ~Shape() {}
+};
+
+class Rect : public Shape
+{
+public:
+	void draw() override { std::cout << "draw rect" << std::endl; }
+};
+class Circle : public Shape
+{
+public:
+	void draw() override { std::cout << "draw circle" << std::endl; }
+};
+
+// 모든 명령의 인터페이스
+struct ICommand
+{
+	virtual void execute() = 0;
+	virtual bool can_undo() { return false; }
+	virtual void undo() { }
+
+	virtual ~ICommand() {}
+};
+
+
+
+int main()
+{
+	std::vector<Shape*> v;
+
+	while (1)
+	{
+		int cmd;
+		std::cin >> cmd;
+
+		if (cmd == 1) v.push_back(new Rect);
+		else if (cmd == 2) v.push_back(new Circle);
+		else if (cmd == 9)
+		{
+			for (auto s : v)
+				s->draw();
+		}
+	}
+}
+
+
+
+
