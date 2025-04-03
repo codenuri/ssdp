4dfb2f67df360003f3165d6a1874cd6584bdee2c
ecourse <smkang@codenuri.co.kr>
2025-04-03 15:30:44 +0900

    Add files via upload

--- DAY4/7_방문자패턴3.cpp
+++ DAY4/7_방문자패턴3.cpp
@@ -2,6 +2,29 @@
 #include <list>
 #include <vector>
 
+// 방문자의 인터페이스
+template<typename T> 
+struct IVisitor
+{
+	virtual void visit(T& e) = 0;
+	virtual ~IVisitor() {}
+};
+
+// 방문자 핵심 : 요소 한개에 대한 연산을 정의 한것
+template<typename T>
+class TwiceVisitor : public IVisitor<T>
+{
+public:
+	void visit(T& e) { e *= 2; }
+};
+
+
+
+
+
+
+
+
 
 int main()
 {
@@ -9,7 +32,6 @@ int main()
 
 	TwiceVisitor<int> tv;
 	s.Accept(&tv);			
-
 
 	ShowVisitor<int> sv;	
 	s.Accept(&sv);
