5219511b19f4782ea3f6142755db4ec5d8494140
ecourse <smkang@codenuri.co.kr>
2025-04-03 10:57:48 +0900

    Add files via upload

--- DAY4/2_Factory6.cpp
+++ DAY4/2_Factory6.cpp
@@ -1,9 +1,21 @@
 ﻿#include <iostream>
 
+// 아래 소스의 핵심
+// => 객체를 만들기 위해 static 멤버 함수를 사용하고 있는것!!
+// => java 에서 이기술을 "static factory method" 라고 합니다
+// => 구글에서 "static factory method" 검색후 1번째 링크.
+//    (복습할때 자세히 읽어 보세요)
+
+
 class Color
 {
 public:
 	Color(int r, int g, int b) {}
+
+	static Color FromRgb(int r, int g, int b)
+	{
+		return Color(r, g, b);
+	}
 };
 
 void fill(const Color& c) {}
@@ -36,4 +48,9 @@ int main()
 
 	// #2. 임시객체로 전달
 	fill( Color(255, 0, 0) ); // 인자전달에만 사용하는 이름없는 객체
+	fill( Color::FromRgb(255, 0, 0));
+
+	fill( Colors::Red() ); // 이름이 있으므로 코드가 보다 명확하다.
+
+	
 }
