ebdc16d7a77354100a4ab82f1dba2b1395920ccd
ecourse <smkang@codenuri.co.kr>
2025-04-03 13:56:45 +0900

    Add files via upload

--- DAY4/6_iterator4.cpp
+++ DAY4/6_iterator4.cpp
@@ -9,6 +9,33 @@ template<typename T> struct Node
 	Node(const T& d, Node* n) : data(d), next(n) {}
 };
 
+// 모든 반복자 클래스가 지켜야 하는 규칙
+template<typename T>
+struct IIterator
+{
+	virtual bool hasNext() = 0;
+	virtual T& next() = 0;
+	virtual ~IIterator() {}
+};
+
+// 모든 컬렉션이 지켜야 하는 규칙(반복자를 꺼낼수 있어야 한다.)
+template<typename T>
+struct ICollection
+{
+	virtual IIterator<T>* iterator() = 0;
+	virtual ~ICollection() {}
+};
+
+
+
+
+
+
+
+
+
+
+
 template<typename T> struct slist
 {
 	Node<T>* head = 0;
