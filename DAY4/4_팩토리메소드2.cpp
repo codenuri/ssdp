0c89dd21e0bcc95339a195bb085642c823a5effc
ecourse <smkang@codenuri.co.kr>
2025-04-03 12:46:00 +0900

    Add files via upload

---
+++ DAY4/4_팩토리메소드2.cpp
@@ -0,0 +1,86 @@
+﻿// 5_팩토리메소드 - 148
+#include <iostream>
+
+// 모든 종류의 컨트롤은 공통의 기반 클래스 필요
+struct IButton
+{
+	virtual void Draw() = 0;
+	virtual ~IButton() {}
+};
+struct IEdit
+{
+	virtual void Draw() = 0;
+	virtual ~IEdit() {}
+};
+
+
+struct WinButton : public IButton
+{
+	void Draw() { std::cout << "Draw WinButton" << std::endl; }
+};
+struct WinEdit : public IEdit
+{
+	void Draw() { std::cout << "Draw WinEdit" << std::endl; }
+};
+
+struct OSXButton : public IButton
+{
+	void Draw() { std::cout << "Draw OSXButton" << std::endl; }
+};
+struct OSXEdit : public IEdit
+{
+	void Draw() { std::cout << "Draw OSXButton" << std::endl; }
+};
+//--------------------------------------
+
+
+class Dialog
+{
+public:
+	void init()
+	{
+		IButton* btn = CreateButton();
+		IEdit* edit = CreateEdit();
+		// btn->Move(); edit->Move();
+		btn->Draw();
+		edit->Draw();
+	}
+
+	virtual IButton* CreateButton() = 0;
+	virtual IEdit*   CreateEdit() = 0;
+};
+
+
+
+class WinDialog
+{
+public:
+
+};
+class OSXDialog
+{
+public:
+	void init()
+	{
+		OSXButton* btn = new OSXButton;
+		OSXEdit* edit = new OSXEdit;
+
+		// btn->Move(); edit->Move();
+
+		btn->Draw();
+		edit->Draw();
+	}
+};
+
+int main(int argc, char** argv)
+{
+
+}
+
+
+
+
+
+
+
+
