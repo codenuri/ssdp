55bfd0f877e7f47c77457e23dc4679ecded4c1ad
ecourse <smkang@codenuri.co.kr>
2025-04-03 09:36:20 +0900

    Add files via upload

---
+++ DAY4/2_Factory2.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+#include <vector>
+#include "Helper.h"
+
+class Shape
+{
+public:
+	virtual void draw() = 0;
+	virtual ~Shape() {}
+};
+
+class Rect : public Shape
+{
+public:
+	void draw() override { std::cout << "draw Rect" << std::endl; }
+
+	// 자신의 객체를 생성하는 static 멤버 함수는 아주 유용하게 활용됩니다.
+	static Shape* create() { return new Rect; }
+};
+
+// Rect 객체를 생성하는 방법
+// 1. Rect* rc = new Rect;			// new 사용
+// 2. Rect* rc = Rect::create();	// static 멤버함수 사용
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Circle : public Shape
+{
+public:
+	void draw() override { std::cout << "draw Circle" << std::endl; }
+};
+
+
+class ShapeFactory
+{
+	MAKE_SINGLETON(ShapeFactory)
+public:
+	Shape* create(int type)
+	{
+		Shape* p = nullptr;
+		if (type == 1)	p = new Rect;
+		else if (type == 2)	p = new Circle;
+		return p;
+	}
+};
+
+
+int main()
+{
+	std::vector<Shape*> v;
+
+	ShapeFactory& factory = ShapeFactory::getInstance();
+
+	while (1)
+	{
+		int cmd;
+		std::cin >> cmd;
+
+
+		if (cmd >= 1 && cmd <= 7) 
+		{
+			Shape* s = factory.create(cmd);
+
+			if (s != nullptr)
+				v.push_back(s);
+		}
+
+		else if (cmd == 9)
+		{
+			for (int i = 0; i < v.size(); i++)
+			{
+				v[i]->draw();
+			}
+		}
+	}
+}
+
+
+
+
