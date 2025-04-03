f1fb7a6d7e701e027104af72c3ccd1c738b0a7c1
ecourse <smkang@codenuri.co.kr>
2025-04-03 12:50:08 +0900

    Add files via upload

--- DAY4/4_팩토리메소드2.cpp
+++ DAY4/4_팩토리메소드2.cpp
@@ -45,31 +45,27 @@ public:
 		btn->Draw();
 		edit->Draw();
 	}
+	// factory method
+	// => 객체를 생성하기 위한 인터페이스를 정의하고, 사용도 하지만
+	// => 어떤 객체를 생성할지는 파생 클래스가 결정한다.
 
+	// => template method 와 동일한 모양이지만
+	// => 가상함수가 하는일이 알고리즘의 교체가 아닌 객체를 생성(종류 결정)
 	virtual IButton* CreateButton() = 0;
 	virtual IEdit*   CreateEdit() = 0;
 };
 
-
-
-class WinDialog
+class WinDialog : public Dialog
 {
 public:
-
+	IButton* CreateButton() override { return new WinButton; }
+	IEdit* CreateEdit()     override { return new WinEdit; }
 };
-class OSXDialog
+class OSXDialog : public Dialog
 {
 public:
-	void init()
-	{
-		OSXButton* btn = new OSXButton;
-		OSXEdit* edit = new OSXEdit;
-
-		// btn->Move(); edit->Move();
-
-		btn->Draw();
-		edit->Draw();
-	}
+	IButton* CreateButton() override { return new OSXButton; }
+	IEdit* CreateEdit()     override { return new OSXEdit; }
 };
 
 int main(int argc, char** argv)
