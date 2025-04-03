935d08cab27ec88b7fbd97cb10c3c3bb130dca85
ecourse <smkang@codenuri.co.kr>
2025-04-03 15:27:01 +0900

    Add files via upload

---
+++ DAY4/7_방문자패턴5.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <conio.h> 
+
+
+class BaseMenu
+{
+	std::string title;
+public:
+	BaseMenu(const std::string& title) : title(title) {}
+	virtual ~BaseMenu() {}
+
+	std::string get_title() const { return title; }
+
+	virtual void command() = 0;
+};
+
+
+class MenuItem : public BaseMenu
+{
+	int id;
+public:
+	MenuItem(const std::string& title, int id) : BaseMenu(title), id(id) {}
+
+	void command() override
+	{
+		std::cout << get_title() << " 메뉴가 선택됨" << std::endl;
+		_getch();
+	}
+};
+
+class PopupMenu : public BaseMenu
+{
+	std::vector<BaseMenu*> v;
+public:
+	PopupMenu(const std::string& title) : BaseMenu(title) {}
+
+	void add_menu(BaseMenu* p) { v.push_back(p); }
+
+	void command() override
+	{
+		while (1)
+		{
+			system("cls");
+
+			int sz = v.size();
+
+			for (int i = 0; i < sz; i++)
+			{
+				std::cout << i + 1 << ". " << v[i]->get_title() << std::endl;
+			}
+
+			std::cout << sz + 1 << ". 종료" << std::endl;
+
+			int cmd;
+			std::cout << "메뉴를 선택하세요 >> ";
+			std::cin >> cmd;
+
+			if (cmd < 1 || cmd > sz + 1)
+				continue;
+
+			if (cmd == sz + 1)
+				break;
+
+			v[cmd - 1]->command(); 
+		}
+
+	}
+
+};
+
+
+
+
+
+
+int main()
+{
+	PopupMenu* root = new PopupMenu("ROOT");
+	PopupMenu* pm1 = new PopupMenu("해상도 변경");
+	PopupMenu* pm2 = new PopupMenu("색상 변경");
+
+	root->add_menu(pm1);
+	root->add_menu(pm2);
+
+
+	pm1->add_menu(new MenuItem("HD", 11));
+	pm1->add_menu(new MenuItem("FHD", 12));
+	pm1->add_menu(new MenuItem("UHD", 13));
+
+	pm2->add_menu(new MenuItem("RED", 21));
+	pm2->add_menu(new MenuItem("GREEN", 22));
+	pm2->add_menu(new MenuItem("BLUE", 23));
+
+
+	root->command();
+
+}
+
+
+
+
