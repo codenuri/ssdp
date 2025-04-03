fd2ec8762f0ac0121d88d5bf438654b838b7c1b6
ecourse <smkang@codenuri.co.kr>
2025-04-03 10:35:39 +0900

    Add files via upload

--- DAY4/2_Factory5.cpp
+++ DAY4/2_Factory5.cpp
@@ -66,8 +66,23 @@ int main()
 
 	ShapeFactory& factory = ShapeFactory::getInstance();
 
-	factory.register_shape(1, &Rect::create);
-	factory.register_shape(2, &Circle::create);
+	// 아래 코드는 공장에 클래스를 등록하는 효과
+	// => 객체를 생성하기위한 정보 등록
+//	factory.register_shape(1, &Rect::create);
+//	factory.register_shape(2, &Circle::create);
+
+
+	// 공장에 클래스가 아닌 자주 사용되는 도형의 견본을 등록해 봅시다.
+	Rect* redRect = new Rect; // 빨간색, 크기 10
+	Rect* blueRect = new Rect; // 파란색색, 크기 20
+	Circle* circle = new Circle; // gradient 의 복잡한 색상의 원
+
+	factory.register_shape(1, redRect);
+	factory.register_shape(2, blueRect);
+	factory.register_shape(3, circle);
+
+
+
 
 
 	while (1)
