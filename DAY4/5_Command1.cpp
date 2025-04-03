234da80fbe593f7f726c517d8c661c407592b65b
ecourse <smkang@codenuri.co.kr>
2025-04-03 16:40:21 +0900

    Add files via upload

--- DAY4/5_Command1.cpp
+++ DAY4/5_Command1.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include "monitor.h"
 
+class ResolutionCommand
+{
+	Monitor& m;
+	int value;
+public:
+	ResolutionCommand(Monitor& m, int v) : m(m), value(v) {}
+
+	void execute() { m.set_resolution(value);}
+};
+
 int main()
 {
 	Monitor m;
 
 	// m 의 해상도를 1920으로 변경하고 싶다.
+
+	// 방법 #1. 멤버 함수를 직접 호출
 	m.set_resolution(1920);
+
+
+	// 방법 #2. Command 패턴을 사용
+	ResolutionCommand cmd(n, 1920);
+	cmd.execute();
 }
