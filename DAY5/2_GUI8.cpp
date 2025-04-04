23063daa76763074c03da4b12a098f075f4931eb
ecourse <smkang@codenuri.co.kr>
2025-04-04 10:15:45 +0900

    Add files via upload

---
+++ DAY5/2_GUI8.cpp
@@ -0,0 +1,97 @@
+#define USING_GUI
+#include "cppmaster.h"
+#include <map>
+#include <vector>
+
+class Window
+{
+	int handle;
+	inline static std::map<int, Window*> this_map;
+
+	std::vector<Window*> child_vec;
+	Window* parent = nullptr;
+public:
+	void add_child(Window* child)
+	{
+		child->parent = this;
+		child_vec.push_back(child);
+		ec_add_child(this->handle, child->handle);
+		ec_set_window_rect(child->handle, 100, 100, 200, 200);
+	}
+
+
+	void create(const std::string& title)
+	{
+		handle = ec_make_window(&event_loop, title);
+
+		this_map[handle] = this;
+	}
+
+	static int event_loop(int hwnd, int msg, int a, int b)
+	{
+		Window* self = this_map[hwnd];
+
+		switch (msg)
+		{
+		case WM_LBUTTONDOWN:
+			self->handle_lbutton_down();
+			break;
+
+		case WM_KEYDOWN:
+			self->key_down();
+			break;
+		}
+		return 0;
+	}
+
+	// 아래 코드가 chain of responsibility 
+	void handle_lbutton_down()
+	{
+		if (lbutton_down() == true)
+			return;
+
+		if (parent != nullptr)				// parent 가 결국 next
+			parent->handle_lbutton_down();
+	}
+
+
+
+
+	virtual void lbutton_down() {}
+	virtual void key_down() {}
+};
+
+
+class MainWindow : public Window
+{
+public:
+	void lbutton_down() override
+	{
+		std::cout << "MainWindow lbutton_down\n";
+	}
+};
+
+class ImageView : public Window
+{
+public:
+	/*
+	void lbutton_down() override
+	{
+		std::cout << "ImageView lbutton_down\n";
+	}
+	*/
+};
+
+int main()
+{
+	MainWindow w;
+	w.create("MainWindow");
+
+	ImageView v;
+	v.create("ImageView");
+
+	w.add_child(&v);
+
+	ec_process_message();
+}
+
