da8f6d42eba1620ac7ec239282adff170dd4c85b
ecourse <smkang@codenuri.co.kr>
2025-04-04 15:23:05 +0900

    Add files via upload

--- DAY5/range2.cpp
+++ DAY5/range2.cpp
@@ -1,34 +1,20 @@
 // range1
 #include <iostream>
 #include <vector>
-
-// 아래 클래스를 잘 생각해 보세요
-template<typename C> class take_view
-{
-	C& c;
-	int cnt;
-public:
-	take_view(C& c, int n) : c(c), cnt(n) {}
-	auto begin() { return c.begin(); }
-	auto end() { return std::next(c.begin(), cnt); }
-};
-
-template<typename C> class reverse_view
-{
-	C& c;
-public:
-	reverse_view(C& c) : c(c) {}
-
-	auto begin() { return c.rbegin(); }
-	auto end() { return c.rend(); }
-};
+#include <ranges>  // 이 안에 take_view 등이 있습니다
+					// 단, C++20 부터 이므로 버전 맞추세요
 
 int main()
 {
 	std::vector v = { 1,2,3,4,5,6,7,8,9,10 };
 
+	// #1. 중첩 허용
+	std::ranges::take_view tv(v, 5);
+	std::ranges::reverse_view rv(tv);
+	std::ranges::filter_view  fv(rv, [](int n) { return n % 2 == 0; });
 
-	for (auto e : rv)
+
+	for (auto e : fv) // 4, 2
 	{
 		std::cout << e << ", ";
 	}
