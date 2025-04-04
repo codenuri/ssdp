21a1274c6bb5b3ac720fc8dcaa6d77a3688a88b8
ecourse <smkang@codenuri.co.kr>
2025-04-04 15:22:20 +0900

    Add files via upload

---
+++ DAY5/range2.cpp
@@ -0,0 +1,37 @@
+// range1
+#include <iostream>
+#include <vector>
+
+// 아래 클래스를 잘 생각해 보세요
+template<typename C> class take_view
+{
+	C& c;
+	int cnt;
+public:
+	take_view(C& c, int n) : c(c), cnt(n) {}
+	auto begin() { return c.begin(); }
+	auto end() { return std::next(c.begin(), cnt); }
+};
+
+template<typename C> class reverse_view
+{
+	C& c;
+public:
+	reverse_view(C& c) : c(c) {}
+
+	auto begin() { return c.rbegin(); }
+	auto end() { return c.rend(); }
+};
+
+int main()
+{
+	std::vector v = { 1,2,3,4,5,6,7,8,9,10 };
+
+
+	for (auto e : rv)
+	{
+		std::cout << e << ", ";
+	}
+
+	std::cout << std::endl;
+}
