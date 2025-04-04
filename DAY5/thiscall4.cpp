0bf718f2c52862b32970f27f44769401f41bc2cc
ecourse <smkang@codenuri.co.kr>
2025-04-04 15:38:42 +0900

    Add files via upload

--- DAY5/thiscall4.cpp
+++ DAY5/thiscall4.cpp
@@ -1,15 +1,31 @@
 #define USING_GUI
 #include "cppmaster.h"
 
-void foo(int id)
-{
-	std::cout << "foo\n";
-}
+
+#include <map>
 
 // C 언어의 타이머 개념을 가지고 클래스를 만들어 봅시다.
 class Clock
 {
+	static std::map<int, Clock*> this_map;
+
+	std::string name;
 public:
+	Clock(const std::string& name) : name(name) {}
+
+	void Start(int ms)
+	{
+		int id = ec_set_timer(ms, handler);
+
+		this_map[id] = this;
+	}
+
+	static void handler(int id)
+	{
+		Clock* self = this_map[id];
+
+		std::cout << self->name << std::endl;
+	}
 };
 
 int main()
