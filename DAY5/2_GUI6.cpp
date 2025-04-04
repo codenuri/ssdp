7b414b3b5fbf4fc6151729437d362b23e59982b6
ecourse <smkang@codenuri.co.kr>
2025-04-04 10:00:07 +0900

    Add files via upload

---
+++ DAY5/2_GUI6.cpp
@@ -0,0 +1,59 @@
+#define USING_GUI
+#include "cppmaster.h"
+#include <map>
+
+
+class Window
+{
+	int handle;
+
+	inline static std::map<int, Window*> this_map; 
+
+public:
+	void create(const std::string& title)
+	{
+		handle = ec_make_window(&event_loop, title);
+
+		this_map[handle] = this;
+	}
+
+	static int event_loop(int hwnd, int msg, int a, int b)
+	{
+		Window* self = this_map[hwnd];
+
+		switch (msg)
+		{
+		case WM_LBUTTONDOWN:
+			self->lbutton_down();
+			break;
+
+		case WM_KEYDOWN:
+			self->key_down();
+			break;
+		}
+		return 0;
+	}
+
+
+	virtual void lbutton_down() {}
+	virtual void key_down() {}
+};
+
+
+class MainWindow : public Window
+{
+public:
+	void lbutton_down() override
+	{
+		std::cout << "MainWindow lbutton_down\n";
+	}
+};
+
+int main()
+{
+	MainWindow w;
+	w.create("A");
+
+	ec_process_message();
+}
+
