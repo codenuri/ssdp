8b3076bb5f5665813783e67479e376a5c503a4ff
ecourse <smkang@codenuri.co.kr>
2025-04-04 13:53:11 +0900

    Add files via upload

---
+++ DAY5/6_Mediator.cpp
@@ -0,0 +1,40 @@
+﻿// Mediator - 171
+#include <iostream>
+
+class CheckBox
+{
+	bool state;
+public:
+	CheckBox() : state(false) {}
+	
+	void SetCheck(bool b) { state = b; ChangeState(); }
+	bool GetCheck()       { return state; }
+
+	virtual void ChangeState() {}
+};
+
+class MyCheckBox : public CheckBox
+{
+public:
+	void ChangeState() override
+	{
+		std::cout << "check box 상태변경\n";
+
+		if (GetCheck() == true)
+		{
+			std::cout << "enable button\n";
+		}
+		else 
+			std::cout << "disable button\n";
+	}
+};
+
+
+int main()
+{
+	MyCheckBox c;
+	c.SetCheck(true);
+}
+
+
+
