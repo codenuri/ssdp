9ebd012eedd0be23bc19e54a1d63c61517e57bf0
ecourse <smkang@codenuri.co.kr>
2026-08-24 11:29:31 +0900

    Add files via upload

---
+++ DAY1/3_가상함수1-3.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+
+class Animal
+{
+	int age;
+public:
+	virtual void f1() {}
+	virtual void f2() {}
+	virtual void f3() {}
+	void f4() {}
+};
+
+class Dog : public Animal
+{
+	int color;
+public:
+	virtual void f2() {}
+};
+//------------------------------------------
+int main()
+{
+	Animal a1, a2;
+	Dog d;
+	Animal* p = &d;
+
+	p->f2();
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
