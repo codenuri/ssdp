b3fc70550785feb2e7ad79b99840bd14c24b3e9e
ecourse <smkang@codenuri.co.kr>
2026-08-24 14:33:49 +0900

    Add files via upload

--- DAY1/5_도형편집기1.cpp
+++ DAY1/5_도형편집기1.cpp
@@ -5,15 +5,30 @@
 
 // 핵심 #1. 모든 도형을 타입으로 설계 합니다.
 
-class Rect
+// 핵심 #2. 모든 도형의 공통의 기반 클래스가 있다면
+// => 장점 #1. color 같은 공통의 멤버를 한곳에서 관리 할수 있게 된다.
+// => 장점 #2. 모든 도형을 하나의 컨테이너에 보관할수있게 된다.
+
+class Shape
+{
+	int color = 0;
+public:
+	virtual ~Shape() {} // 가상 소멸자
+};
+class Rect : public Shape
 {
 	// x, y, w, h 생성자등은 생략
 public:
 	void draw() { std::cout << "draw Rect\n"; }
 };
+class Circle : public Shape
+{
+public:
+	void draw() { std::cout << "draw Circle\n"; }
+};
 
 int main()
 {
-	std::vector<Rect*> v;
+	std::vector<Rect*> v;	// Rect 만 보관 가능한 vector
 }
 
