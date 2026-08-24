040de72c88e6b4c6b5cc544342f9b1ca05e05168
ecourse <smkang@codenuri.co.kr>
2026-08-24 15:21:20 +0900

    Add files via upload

---
+++ DAY1/5_도형편집기3.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <vector>
+
+
+class Shape
+{
+	int color = 0;
+public:
+	virtual ~Shape() {}
+
+	void set_color(int c) { color = c; }
+
+	virtual int get_area() { return -1; }
+
+	virtual void draw() { std::cout << "draw Shape\n"; }
+};
+
+
+
+class Rect : public Shape
+{
+public:
+	void draw() { std::cout << "draw Rect\n"; }
+};
+
+class Circle : public Shape
+{
+public:
+	void draw() { std::cout << "draw Circle\n"; }
+};
+
+int main()
+{
+	std::vector<Shape*> v;
+
+	while (1)
+	{
+		int cmd;
+		std::cin >> cmd;
+
+		if (cmd == 1) v.push_back(new Rect);
+		else if (cmd == 2) v.push_back(new Circle);
+		else if (cmd == 9)
+		{
+			for (auto s : v)
+				s->draw();
+
+		}
+	}
+}
+
+
+
+
+
+
