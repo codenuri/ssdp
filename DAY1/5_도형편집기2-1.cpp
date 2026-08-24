6a21871343e8d8aa9f5de7f889ab838130c7faab
ecourse <smkang@codenuri.co.kr>
2026-08-24 14:43:36 +0900

    Add files via upload

---
+++ DAY1/5_도형편집기2-1.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <vector>
+
+
+class Shape
+{
+	int color = 0;
+public:
+	virtual ~Shape() {}
+};
+
+class Rect : public Shape
+{
+public:
+	void draw() { std::cout << "draw Rect\n"; }
+};
+
+class Circle : public Shape
+{
+public:
+	void draw() { std::cout << "draw Circle\n"; }
+};
+
+int main()
+{
+	std::vector<Shape*> v;
+
+	while (1)
+	{
+		int cmd;
+		std::cin >> cmd;
+
+		if (cmd == 1) v.push_back(new Rect);
+		else if (cmd == 2) v.push_back(new Circle);
+		else if (cmd == 9)
+		{
+			for (auto s : v)
+			{
+				// 해결책 #1. 캐스팅
+				// => Shape* 인 s 를 Rect*, Circle* 로 캐스팅 후에 사용
+
+
+				s->draw();
+			}
+		
+		}
+	}
+}
+
+
+
+
+
+
