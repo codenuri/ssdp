feb4ad58973292cd5bc99c897ed845e8d874d20f
ecourse <smkang@codenuri.co.kr>
2026-08-24 16:02:10 +0900

    Add files via upload

---
+++ DAY1/5_도형편집기6.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <vector>
+
+class Shape
+{
+	int color = 0;
+public:
+	virtual ~Shape() {}
+	void set_color(int c) { color = c; }
+
+	void draw()
+	{
+		std::cout << "mutex.lock\n";
+		draw_imp();
+		std::cout << "mutex.unlock\n";
+	}
+
+	virtual void draw_imp()
+	{
+		std::cout << "draw Shape\n";
+	}
+
+	virtual Shape* clone() { return new Shape(*this); }
+
+
+	virtual int get_area() { return -1; }
+
+};
+
+
+
+class Rect : public Shape
+{
+public:
+	void draw_imp() { std::cout << "draw Rect\n"; }
+	Shape* clone() { return new Rect(*this); }
+};
+
+class Circle : public Shape
+{
+public:
+	void draw_imp() { std::cout << "draw Circle\n"; }
+	Shape* clone() { return new Circle(*this); }
+};
+
+int main()
+{
+	std::vector<Shape*> v;
+
+	while (1)
+	{
+		int cmd;
+		std::cin >> cmd;
+
+		if (cmd == 1) v.push_back(new Rect);
+		else if (cmd == 2) v.push_back(new Circle);
+		else if (cmd == 9)
+		{
+			for (auto s : v)
+			{
+				s->draw();
+			}
+		}
+
+		else if (cmd == 8)
+		{
+			std::cout << "몇 번째 도형을 복제 할까요 >> ";
+			int k;
+			std::cin >> k;
+
+			v.push_back(v[k]->clone());
+
+		}
+
+	}
+}
+
+
+
+
+
+
