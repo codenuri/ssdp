5520d6528c3305d561b88ec27243400a059fc595
ecourse <smkang@codenuri.co.kr>
2026-08-24 13:52:39 +0900

    Add files via upload

--- DAY1/4_추상클래스3.cpp
+++ DAY1/4_추상클래스3.cpp
@@ -13,35 +13,33 @@ public:
 	virtual void take() = 0;
 };
 
+// 실제 카메라는 없지만 규칙이 있습니다.
+// 사용자는 실제 카메라에 대해서는 몰라도 됩니다.
+// 오직 규칙만 알면 되고, 규칙대로만 사용하면 됩니다.
+// => 아래 People 은 실제 카메라 제품의 이름(클래스이름)은 사용하지 않습니다
+class People
+{
+public:
+	// 인자로 규칙의 이름을 사용합니다
+	void use_camera(ICamera* p) { p->take(); }
+};
 
 
+// 이제 모든 카메라는 규칙대로 설계되어야 합니다.
+// 1. ICamera 로 부터 상속 받고
+// 2. take() 를 만들어야 한다
 
-
-
-
-
-
-
-
-
-
-
-class Camera
+class Camera : public ICamera
 {
 public:
 	void take() { std::cout << "Take Picture" << std::endl; }
 };
-class HDCamera
+class HDCamera : public ICamera
 {
 public:
 	void take() { std::cout << "Take HD Picture" << std::endl; }
 };
-class People
-{
-public:
-	void use_camera(Camera* p) { p->take(); }
-	void use_camera(HDCamera* p) { p->take(); }
-};
+
 int main()
 {
 	People p;
