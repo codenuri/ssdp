c5a86f76ce2d4b7fa28d018435d03a23594f406e
ecourse <smkang@codenuri.co.kr>
2026-08-24 10:25:06 +0900

    Add files via upload

---
+++ DAY1/2_upcasting3.cpp
@@ -0,0 +1,38 @@
+﻿// upcasting3.cpp
+class Animal 
+{
+public: 
+	int age;
+};
+class Cat : public Animal 
+{
+};
+class Dog : public Animal
+{
+public:
+	int color;
+};
+
+// upcasting 장점(활용) #2. 동종을 처리하는 함수 만들기 
+//void NewYear(Dog* pDog)	// 인자로 Dog 객체만 전달 가능한 함수
+void NewYear(Animal* p)		// 인자로 모든 동물을 전달 가능한 함수
+{
+	++(p->age); 
+	
+}
+int main()
+{
+	Animal a; NewYear(&a);
+	Dog    d; NewYear(&d);
+}
+
+
+
+
+
+
+
+
+
+
+
