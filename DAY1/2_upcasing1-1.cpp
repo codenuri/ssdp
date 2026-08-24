187b45a90b41263b3a5f201bc36e06995b49b106
ecourse <smkang@codenuri.co.kr>
2026-08-24 10:04:02 +0900

    Add files via upload

---
+++ DAY1/2_upcasing1-1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+class Animal
+{
+public:
+	int age;
+};
+class Dog : public Animal
+{
+public:
+	int color;
+};
+int main()
+{
+	Dog d;
+	Animal a;
+	
+	Animal* p = &a; 
+
+	// 현재 p가 가리키는 대상은 Animal !
+
+	Dog* p1 = static_cast<Dog*>(p);
+
+	std::cout << p1 << '\n';
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
