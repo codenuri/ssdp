dced443559a83d4448ed85ce41997cc314c90479
ecourse <smkang@codenuri.co.kr>
2026-08-24 10:08:02 +0900

    Add files via upload

--- DAY1/2_upcasing1-1.cpp
+++ DAY1/2_upcasing1-1.cpp
@@ -18,9 +18,21 @@ int main()
 
 	// 현재 p가 가리키는 대상은 Animal !
 
+	// static_cast : 대상체가 Dog 인지 조사할수 없다
+	//				 아래 코드는 항상 유효한 주소 반환
+	//				 만일 대상체가 Dog 가 아니라면 반환된 p1을 
+	//				 "p1->color = 10" 으로 사용하면 잘못된 메모리 참조
 	Dog* p1 = static_cast<Dog*>(p);
 
 	std::cout << p1 << '\n';
+
+
+	// dynamic_cast : 실행시간 캐스팅
+	//				  실행시간에 대상체가 Dog 인지 확인후 캐스팅
+	//				  Dog 가 아니라면 0 반환
+	Dog* p2 = dynamic_cast<Dog*>(p);
+
+	std::cout << p2 << '\n';
 
 
 }
