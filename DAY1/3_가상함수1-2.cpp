8e526aaf573084a383a46096b775bd09657a3bc5
ecourse <smkang@codenuri.co.kr>
2026-08-24 11:10:10 +0900

    Add files via upload

---
+++ DAY1/3_가상함수1-2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+// 가상함수의 호출의 정확한 원리
+
+class Animal
+{
+public:
+	virtual void f1() {}
+	virtual void f2() {}
+	virtual void f3() {}
+	void f4() {}
+};
+
+class Dog : public Animal
+{
+public:
+	virtual void f2() {}
+};
+int main()
+{
+	Animal a;
+	Dog d;
+	Animal* p = &d;
+
+	p->f2();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
