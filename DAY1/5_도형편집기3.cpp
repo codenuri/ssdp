dccb249f7ef02c3613f0e1d1171eed2b8da37ce1
ecourse <smkang@codenuri.co.kr>
2026-08-24 15:27:53 +0900

    Add files via upload

--- DAY1/5_도형편집기3.cpp
+++ DAY1/5_도형편집기3.cpp
@@ -1,6 +1,19 @@
 ﻿#include <iostream>
 #include <vector>
 
+// 핵심 #5. 다형성(Polymorphism)
+// => OCP 를 만족하는 좋은 코딩 스타일
+
+
+// 핵심 #6. 아래 "리팩토링 용어" 를 이해해 보세요
+// 
+// => "Replace Conditional With Polymorphism"
+//    "제어문(if, switch)이 많다면 다형성(가상함수) 로 변경하는 것을 고려하라"
+
+
+
+
+
 
 class Shape
 {
@@ -28,7 +41,11 @@ class Circle : public Shape
 public:
 	void draw() { std::cout << "draw Circle\n"; }
 };
-
+class Triangle : public Shape
+{
+public:
+	void draw() { std::cout << "draw Triangle\n"; }
+};
 int main()
 {
 	std::vector<Shape*> v;
@@ -43,7 +60,13 @@ int main()
 		else if (cmd == 9)
 		{
 			for (auto s : v)
-				s->draw();
+			{
+				s->draw(); // 다형성(Polymorphism)
+							// => 동일한 하나의 표현식이
+							//    상황(실제 객체가 무엇인가?)에 따라 다르게 동작하는 것
+
+				// 객체지향의 3대 특징 : 캡슐화, 상속, 다형성
+			}
 
 		}
 	}
