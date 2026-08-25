ec4fa2accb8697d91cc9b82ed538b30603e1f2cf
ecourse <smkang@codenuri.co.kr>
2026-08-25 15:10:57 +0900

    Add files via upload

--- DAY2/this1.cpp
+++ DAY2/this1.cpp
@@ -3,10 +3,10 @@ class Point
 {
 	int x, y;
 public:
-	void set(int a, int b)
+	void set(int a, int b) // void set(Point* this, int a, int b)
 	{
-		x = a;
-		y = b;
+		x = a;		// this->x = a;
+		y = b;		// this->y = b;
 	}
 };
 
@@ -14,6 +14,6 @@ int main()
 {
 	Point p1;
 	Point p2;
-	p1.set(1, 2); // 이 한줄의 원리를 생각해 봅시다
-	p2.set(1, 2);
+	p1.set(1, 2); // set(&p1, 1, 2)  이 한줄의 원리를 생각해 봅시다
+	p2.set(1, 2); // set(&p2, 1, 2)
 }
