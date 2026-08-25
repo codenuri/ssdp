b1d2cb5b70cb2ca3e0bfa2376964f687e25e3a62
ecourse <smkang@codenuri.co.kr>
2026-08-25 14:32:51 +0900

    Add files via upload

---
+++ DAY2/5_menu_event5.cpp
@@ -0,0 +1,116 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <conio.h> 
+#include <functional>
+
+class BaseMenu
+{
+	std::string title;
+public:
+	BaseMenu(const std::string& title) : title(title) {}
+
+	virtual ~BaseMenu() {}
+
+	std::string get_title() const { return title; }
+
+	virtual void command() = 0;
+};
+
+
+
+
+class MenuItem : public BaseMenu
+{
+	int id;
+
+	using HANDLER = std::function<void()>; 
+
+	std::vector< HANDLER > handler_vector;
+
+public:
+	MenuItem(const std::string& title, int id, HANDLER h = 0)
+		: BaseMenu(title), id(id)
+	{
+		if (h != 0)
+			handler_vector.push_back(h);
+	}
+
+	void add_handler(HANDLER h) { handler_vector.push_back(h); }
+
+	void command()
+	{
+		for (auto f : handler_vector)
+		{
+			f();
+		}
+	}
+};
+
+class PopupMenu : public BaseMenu
+{
+	std::vector<BaseMenu*> v;
+
+public:
+	PopupMenu(const std::string& title) : BaseMenu(title) {}
+
+	void add(BaseMenu* m) { v.push_back(m); }
+
+	void command()
+	{
+		while (1)
+		{
+			system("cls");
+
+			auto sz = v.size();
+
+			for (int i = 0; i < sz; i++)
+			{
+				std::cout << i + 1 << ". " << v[i]->get_title() << '\n';
+			}
+
+			std::cout << sz + 1 << ". 종료\n";
+
+			std::cout << "메뉴를 선택하세요 >> ";
+
+			int cmd;
+			std::cin >> cmd;
+
+
+			if (cmd == sz + 1)
+				break;
+
+			if (cmd < 0 || cmd > sz + 1)
+				continue;
+
+			v[cmd - 1]->command();
+
+		}
+
+	}
+
+};
+
+int main()
+{
+	PopupMenu* root = new PopupMenu("ROOT");
+	PopupMenu* p1 = new PopupMenu("해상도 변경");
+	PopupMenu* p2 = new PopupMenu("색상 변경");
+
+	root->add(p1);
+	root->add(p2); // root 하위 메뉴로 부착
+
+	p1->add(new MenuItem("HD", 11));
+	p1->add(new MenuItem("FHD", 12));
+	p1->add(new MenuItem("UHD", 13));
+
+	p2->add(new MenuItem("RED", 21));
+	p2->add(new MenuItem("GREEN", 22));
+	p2->add(new MenuItem("BLUE", 23));
+	p2->add(new MenuItem("WHITE", 24));
+
+	root->command();
+
+}
+
+
