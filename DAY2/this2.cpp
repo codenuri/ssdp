471db1cc47237267d7e20d27ac4bdd432b616c99
ecourse <smkang@codenuri.co.kr>
2026-08-25 15:15:01 +0900

    Add files via upload

--- DAY2/this2.cpp
+++ DAY2/this2.cpp
@@ -2,6 +2,7 @@
 {
 public:
 	void close(int a) {}
+	static void close2(int a) {}
 };
 
 void foo(int a) {}
@@ -9,5 +10,12 @@ void foo(int a) {}
 int main()
 {
 	void(*f1)(int) = &foo; // ok
-	void(*f1)(int) = &Dialog::close; // ?? 될까요 ?
+
+	void(*f2)(int) = &Dialog::close; // error. close 는 인자가 한개가 아닙니다
+										// this 가 있습니다. 그래서 error
+	void(*f3)(int) = &Dialog::close2; // ok. static 멤버 함수는 this 없음.
+
+	// 핵심 :
+	// 1. 멤버 함수의 주소를 일반 함수 포인터에 담을수 없다.
+	// 2. static 멤버 함수의 주소를 일반 함수 포인터에 담을수 있다.
 }
