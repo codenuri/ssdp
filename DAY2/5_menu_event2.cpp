26ace16871fe28558598eb6150ffa1d894da9787
ecourse <smkang@codenuri.co.kr>
2026-08-25 14:11:58 +0900

    Add files via upload

---
+++ DAY2/5_menu_event2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <functional> 
+using namespace std::placeholders; 
+
+void foo()
+{
+	std::cout << "foo\n";
+}
+
+int main()
+{
+	// C 언어 함수 포인터 : "함수의 주소" 를 보관하는 타입
+	void(*f)() = &foo;
+}
