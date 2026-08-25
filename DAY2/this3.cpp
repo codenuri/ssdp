8e02748bc2b08c27f9cb3498d4114fb7dc91d5d7
ecourse <smkang@codenuri.co.kr>
2026-08-25 15:16:07 +0900

    Add files via upload

---
+++ DAY2/this3.cpp
@@ -0,0 +1,16 @@
+﻿class Dialog
+{
+public:
+	void close(int a) {}
+};
+
+void foo(int a) {}
+
+int main()
+{
+	void(*f1)(int) = &foo; // ok
+	
+	// 핵심 #1. 멤버 함수의 주소를 담는 포인터 만들기
+
+	void(*f2)(int) = &Dialog::close; 
+}
