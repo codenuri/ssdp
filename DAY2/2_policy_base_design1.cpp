75d67d6aac485ea8d3c31a3f0b18c0aedebaf76d
ecourse <smkang@codenuri.co.kr>
2026-08-25 09:00:34 +0900

    Add files via upload

---
+++ DAY2/2_policy_base_design1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+// policy base design ( 단위전략 디자인 ) - 42p ~ 
+
+template<typename T>
+class vector
+{
+	T* ptr = nullptr;
+	std::size_t sz;
+public:
+	void resize(std::size_t newsize)
+	{
+		delete[] ptr;			// 기존 버퍼를 제거하고
+
+		ptr = new T[newsize];	// 새로운 버퍼를 할당
+
+		sz = newsize;
+	}
+};
+
+int main()
+{
+	vector<int> v;
+	v.resize(10);
+}
+
+// 위 코드의 문제점
+// => vector 라는 클래스의 메모리 할당 방법을 변경할수 없습니다.
+//    (항상 new/delete 로만 할당/해지 됩니다.)
+// => C++ 에는 다양한 메모리 할당 방법이 있습니다.
+//    (new / malloc / system call(windows api) / memory pooling)
---
+++ DAY2/2_policy_base_design2.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+
+// vector 의 메모리 할당 방법을 사용자가 변경할수 있도록 만들어 봅시다
+
+// 방법 #1. template method 패턴
+
+
+
+
+
+template<typename T>
+class vector
+{
+	T* ptr = nullptr;
+	std::size_t sz;
+public:
+	void resize(std::size_t newsize)
+	{
+//		delete[] ptr;			
+		deallocate(ptr, sz);
+
+//		ptr = new T[newsize];	
+		ptr = allocate(newsize);
+
+		sz = newsize;
+	}
+
+	// 메모리 할당/해지 를 책임지는 가상함수
+	virtual T* allocate(std::size_t sz)
+	{
+		return new T[sz];
+	}
+	virtual void deallocate(T* p, std::size_t sz)
+	{
+		delete[] p;
+	}
+};
+
+// 위 설계의 의도
+// => 메모리 할당/해지 방법을 변경하려면 "파생클래스를 만들어서"
+// => 약속된 가상함수를 override
+template<typename T> class malloc_vector : public vector<T>
+{
+public:
+	T* allocate(std::size_t sz) override
+	{
+		void* ptr = malloc(sizeof(T) * sz);
+		return static_cast<T*>(ptr);
+	}
+	void deallocate(T* p, std::size_t sz) override
+	{
+		free(p);
+	}
+};
+int main()
+{
+//	vector<int> v;
+	malloc_vector<int> v;
+	v.resize(10);	
+}
