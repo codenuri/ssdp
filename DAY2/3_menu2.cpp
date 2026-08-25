f40f45e45bc2e17f1d58696d197b777efcffe64f
ecourse <smkang@codenuri.co.kr>
2026-08-25 10:48:18 +0900

    Add files via upload

---
+++ DAY2/3_menu2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <conio.h> 
+
+// 객체지향 프로그램
+// => 필요한 타입을 먼저 만든다.
+// => 메뉴 관련 프로그램을 작성한다면 "Menu" 라는 타입을 먼저 만든다
+
+class MenuItem
+{
+
+};
+
+int main()
+{
+	MenuItem m1("김밥", 11);
+	MenuItem m2("라면", 12);
+
+	m1.command(); // 김밥 선택
+}
+
+
+
+
