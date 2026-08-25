52980da03901a0d24a6c2a43a486244e987d9fcf
ecourse <smkang@codenuri.co.kr>
2026-08-25 15:07:30 +0900

    Add files via upload

---
+++ DAY2/this1.cpp
@@ -0,0 +1,19 @@
+﻿// this1.cpp
+class Point
+{
+	int x, y;
+public:
+	void set(int a, int b)
+	{
+		x = a;
+		y = b;
+	}
+};
+
+int main()
+{
+	Point p1;
+	Point p2;
+	p1.set(1, 2); // 이 한줄의 원리를 생각해 봅시다
+	p2.set(1, 2);
+}
