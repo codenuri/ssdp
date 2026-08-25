472ce9405e7e853804fc3c17cff6a5af5f627918
ecourse <smkang@codenuri.co.kr>
2026-08-25 11:43:18 +0900

    Add files via upload

--- DAY2/3_menu6.cpp
+++ DAY2/3_menu6.cpp
@@ -85,7 +85,8 @@ int main()
 	PopupMenu* p2 = new PopupMenu("색상 변경");
 
 	root->add(p1);
-	root->add(p2);
+//	root->add(p2); // root 하위 메뉴로 부착
+	p1->add(p2);   // 해상도의 하위 메뉴로 색상 부착
 
 	p1->add(new MenuItem("HD",  11));
 	p1->add(new MenuItem("FHD", 12));
@@ -94,6 +95,7 @@ int main()
 	p2->add(new MenuItem("RED",   21));
 	p2->add(new MenuItem("GREEN", 22));
 	p2->add(new MenuItem("BLUE",  23));
+	p2->add(new MenuItem("WHITE", 24));
 
 	// #1. 위 메뉴 객체들의 관계를 그림을 표현해 보세요 - 어떤 자료구조와 유사합니다
 	// => 객체의 포함관계가 "tree" 처럼 구성 된다
@@ -103,5 +105,17 @@ int main()
 }
 
 
+// 객체지향 프로그래밍 관점에서 "프로그램 이란?"
+
+// 1. 객체를 생성하고
+// 2. 객체 간의 관계를 설정하고
+// 3. 객체 간의 메세지를 주고 받는 과정이다
+
+// 4. 프로그램의 기본 요소는 "함수" 가 아닌 "클래스" 이다
+
+// 장점 : 유지보수가 쉽고, 확장성이 있다
+
+// 단점 : C 보다 메모리 사용량 증가, 약간의 오버헤드가 있을수 있다
+//        => 가상함수등에서... 
 
 
