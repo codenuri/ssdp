bbcf648d625bdc71d695617ad7e3af5f2a012430
ecourse <smkang@codenuri.co.kr>
2026-08-26 09:56:08 +0900

    Add files via upload

--- DAY3/1_Adapter6.cpp
+++ DAY3/1_Adapter6.cpp
@@ -8,8 +8,9 @@
 //				 => 기능과 인터페이스 모두 상속
 // 
 
-// private 상속
-
+// private 상속 : 기반 클래스의 모든 멤버를 private 으로 상속하겠다는 것
+// => 구현은 물려 받지만(stack 이 내부에서 list 기능 사용)
+// => 인터페이스는 물려 받지 않겠다(stack 객체로 list 기능을 외부 노출하지 않겠다)
 
 template<typename T>
 class stack : private std::list<T>
@@ -28,5 +29,7 @@ int main()
 
 	auto n = s.top();
 
-	s.push_front(5);
+	s.push_front(5); // error. private 상속이므로
+					 // list 의 모든 멤버 함수는 stack 객체를 사용해서는
+					 // 호출 할수 없다 (private 멤버)
 }
