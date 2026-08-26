970e4bff0eb803d9efe49cc4d1a6a0eeb1386c38
ecourse <smkang@codenuri.co.kr>
2026-08-26 13:33:29 +0900

    Add files via upload

--- DAY3/ICalc.h
+++ DAY3/ICalc.h
@@ -1,13 +1,40 @@
 ﻿#pragma once
 
-// ICalc.h
-struct ICalc
+// 서버가 여러종류이고, 각각의 Proxy 가 있을때
+// 모두 Reference Counting 기술을 사용하면
+// 결국 AddRef(), Release() 는 모두 필요 합니다.
+
+struct IRefCount
 {
+	// 참조계수로 객체의 수명을 관리하는 경우
+	// => 참조계수용 함수도 반드시 인터페이스에 있어야 합니다
+	virtual void AddRef() = 0;
+	virtual void Release() = 0;
+
+	virtual ~IRefCount() {}
+};
+
+// ICalc.h
+struct ICalc : public IRefCount 
+{
+
+
+
+
 	virtual int Add(int a, int b) = 0;
 	virtual int Sub(int a, int b) = 0;
 
 	virtual ~ICalc() {}
 };
+
+
+
+
+
+
+
+
+
 
 ICalc* load_proxy()
 {
