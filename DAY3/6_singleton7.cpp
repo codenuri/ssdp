327e7d6d3e7ccb984fee43495beb4977cf66439d
ecourse <smkang@codenuri.co.kr>
2026-08-26 16:45:21 +0900

    Add files via upload

--- DAY3/6_singleton7.cpp
+++ DAY3/6_singleton7.cpp
@@ -1,6 +1,11 @@
 ﻿#include <iostream>
 #include <mutex>
 
+// CRTP
+// => 기반 클래스 만들때 미래에 만들어질 파생 클래스의 이름을 
+// => 기반 클래스 내부에서 사용하는 기술
+
+template<typename T>
 class Singleton
 {
 private:
@@ -10,25 +15,22 @@ private:
 
 	// inline static 문법 : static 멤버 데이타의 외부 선언이 없어도 됩니다 - C++17 부터
 	inline static std::mutex mtx;
-	inline static Singleton* sinstance;
+	inline static T* sinstance;
 public:
-	static Singleton& get_instance()
+	static T& get_instance()
 	{
 		std::lock_guard<std::mutex> g(mtx);
 
 		if (sinstance == nullptr)
 		{
-			sinstance = new Singleton;
+			sinstance = new T;
 		}
 
 		return *sinstance;
 	}
 };
-
-
 // Keyboard 클래스도 위와 같은 Singleton 기술을 사용하고 싶다
-
-class Keybooard : public Singleton
+class Keyboard : public Singleton< Keyboard >
 {
 
 };
@@ -36,8 +38,12 @@ class Keybooard : public Singleton
 
 int main()
 {
-	Cursor& c1 = Cursor::get_instance();
-	Cursor& c2 = Cursor::get_instance();
+	Keyboard& k1 = Keyboard::get_instance();
+	Keyboard& k2 = Keyboard::get_instance();
+
+	std::cout << &k1 << std::endl;
+	std::cout << &k2 << std::endl;
+
 }
 
 
