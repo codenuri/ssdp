0f117c0403fd2cacf4e25aaec60cedb251616428
ecourse <smkang@codenuri.co.kr>
2026-08-26 10:03:20 +0900

    Add files via upload

---
+++ DAY3/1_Adapter8.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <list>
+#include <vector>
+#include <deque>
+
+template<typename T>
+class stack
+{
+	std::list<T> c;
+public:
+	void push(const T& e)	{ c.push_back(e); }
+	void pop()				{ c.pop_back(); }
+	T& top()				{ return c.back(); }
+};
+
+int main()
+{
+	stack<int> s;
+	s.push(10);
+	s.push(20);
+
+	auto n = s.top();
+}
