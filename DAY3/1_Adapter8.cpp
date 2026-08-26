ff47900c49d284932de45928a7a4edbbcc3451f4
ecourse <smkang@codenuri.co.kr>
2026-08-26 10:10:57 +0900

    Add files via upload

--- DAY3/1_Adapter8.cpp
+++ DAY3/1_Adapter8.cpp
@@ -14,9 +14,9 @@ class stack
 //	std::list<T> c;
 	C c;
 public:
-	void push(const T& e)	{ c.push_back(e); }
-	void pop()				{ c.pop_back(); }
-	T& top()				{ return c.back(); }
+	inline void push(const T& e)	{ c.push_back(e); }
+	inline void pop()				{ c.pop_back(); }
+	inline T& top()				{ return c.back(); }
 };
 int main()
 {
