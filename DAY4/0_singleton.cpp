59f2b596b6283ecf80ec07ff5aceca51bf78c8f8
ecourse <smkang@codenuri.co.kr>
2026-08-27 08:37:53 +0900

    Add files via upload

---
+++ DAY4/0_singleton.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+#define MAKE_SINGLETON(classname)				\
+private:										\
+	classname() {}								\
+	classname(const classname&) = delete;		\
+	void operator=(const classname&) = delete;	\
+public:											\
+	static classname& get_instance()			\
+	{											\
+		static classname instance;				\
+		return instance;						\
+	}											\
+private:
+
+class Cursor
+{
+	MAKE_SINGLETON(Cursor)
+};
+
+int main()
+{
+	Cursor& c = Cursor::get_instance();
+}
+
---
+++ DAY4/singleton.h
@@ -0,0 +1,17 @@
+﻿// singleton.h
+#pragma once
+
+#define MAKE_SINGLETON(classname)			\
+private:									\
+	classname() {}							\
+public:										\
+	static classname& get_instance()		\
+	{										\
+	static classname instance;				\
+		return instance;					\
+	}										\
+public:										\
+	classname(classname&) = delete;			\
+	void operator=(classname&) = delete;	\
+private:
+
