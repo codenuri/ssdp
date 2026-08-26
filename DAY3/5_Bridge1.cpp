5d3503aa4b9347e1aa44adbdf9aea757c15dcc77
ecourse <smkang@codenuri.co.kr>
2026-08-26 14:21:39 +0900

    Add files via upload

---
+++ DAY3/5_Bridge1.cpp
@@ -0,0 +1,25 @@
+﻿// 6_Bridge1 - 90 page
+#include <iostream>
+
+class MediaPlayer 
+{
+public:
+	void play() { std::cout << "using FFmpeg\n"; }
+};
+
+class People
+{
+public:
+	void use(MediaPlayer* p)
+	{
+		p->play();
+	}
+};
+int main()
+{
+	People p;
+	MediaPlayer player;
+	p.use(&player);
+}
+
+
