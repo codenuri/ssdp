ffc2719b011f803c049d3a466d0186a97c428a41
ecourse <smkang@codenuri.co.kr>
2026-08-26 13:46:53 +0900

    Add files via upload

--- DAY3/3_RemoteProxy8.cpp
+++ DAY3/3_RemoteProxy8.cpp
@@ -7,8 +7,25 @@
 // => 사용자가 직접 참조계수를 관리해야 하므로 "단순 코드의 반복"
 // => 그리고, "실수 할수 있다" AddRef()/Release() 를 실수로 호출 안함
 
-// 
+// 해결책
+// "Raw Pointer" 를 직접 사용하지 말고 "대신 사용할것" 을 만들어 봅시다 - proxy 패턴
+// => 스마트 포인터 도입
 
+template<typename T>
+class sp
+{
+	T* obj;
+public:
+	sp(T* p = nullptr) : obj(p) {} 
+	sp(const sp& other) : obj(other.obj) {}
+	~sp() {}
+};
+
+int main()
+{
+	sp<ICalc> calc1 = load_proxy(); // sp<ICalc> calc1(load_proxy()); 
+	sp<ICalc> calc2 = calc1;
+}
 
 int main()
 {
