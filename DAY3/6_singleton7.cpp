1a49536d603187e0a5dcc11eff1cfb46bfb8162b
ecourse <smkang@codenuri.co.kr>
2026-08-26 16:37:07 +0900

    Add files via upload

---
+++ DAY3/6_singleton7.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <mutex>
+
+class Cursor
+{
+private:
+	Cursor() {}
+	Cursor(const Cursor&) = delete;
+	Cursor operator=(const Cursor&) = delete;
+
+	static std::mutex mtx;
+	static Cursor* sinstance;
+public:
+
+	static Cursor& get_instance()
+	{
+		std::lock_guard<std::mutex> g(mtx);
+
+		if (sinstance == nullptr)
+		{
+			sinstance = new Cursor;
+		}
+
+		return *sinstance;
+	}
+};
+Cursor* Cursor::sinstance = nullptr;
+std::mutex Cursor::mtx;
+
+
+
+int main()
+{
+	Cursor& c1 = Cursor::get_instance();
+	Cursor& c2 = Cursor::get_instance();
+}
+
+
+
+
+
+
+
+
