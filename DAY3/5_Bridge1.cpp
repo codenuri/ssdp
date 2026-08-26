f308c2a770cf751517054215ad6955709fabb569
ecourse <smkang@codenuri.co.kr>
2026-08-26 14:40:14 +0900

    Add files via upload

--- DAY3/5_Bridge1.cpp
+++ DAY3/5_Bridge1.cpp
@@ -1,16 +1,30 @@
 ﻿// 6_Bridge1 - 90 page
 #include <iostream>
+// 다양한 Codec 을 활용가능하도록 인터페이스를 먼저 설계
+struct IMediaPlayer
+{
+	virtual void play() = 0;
+	virtual ~IMediaPlayer() {}
+};
 
-class MediaPlayer 
+// 음악을 play 하는 클래스
+class FFmpegMediaPlayer : public IMediaPlayer
 {
 public:
 	void play() { std::cout << "using FFmpeg\n"; }
 };
+class HardwareMediaPlayer : public IMediaPlayer
+{
+public:
+	void play() { std::cout << "using Hardware Codec\n"; }
+};
+
+
 
 class People
 {
 public:
-	void use(MediaPlayer* p)
+	void use(IMediaPlayer* p)
 	{
 		p->play();
 	}
@@ -18,7 +32,7 @@ public:
 int main()
 {
 	People p;
-	MediaPlayer player;
+	FFmpegMediaPlayer player;
 	p.use(&player);
 }
 
