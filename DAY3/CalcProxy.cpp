d84d5d09dcb861e94e931ced1db944634df32f28
ecourse <smkang@codenuri.co.kr>
2026-08-26 11:45:47 +0900

    Add files via upload

--- DAY3/CalcProxy.cpp
+++ DAY3/CalcProxy.cpp
@@ -2,6 +2,7 @@
 
 #define USING_GUI
 #include "cppmaster.h"
+
 #include "ICalc.h"
 
 class Calc : public ICalc
@@ -13,3 +14,21 @@ public:
 	int Add(int a, int b) { return ec_send_server(server, 1, a, b); }
 	int Sub(int a, int b) { return ec_send_server(server, 2, a, b); }
 };
+
+// Client 개발자는 Proxy 클래스의 이름을 
+// 알수 없고, 알 필요도 없고, 사용해도 안됩니다.
+// DLL 내부에 약속된 함수를 제공하고 해당 함수에서 Calc 객체 생성
+extern "C" __declspec(dllexport)	// windows DLL 에서 함수 만들때 필요한것
+ICalc* create()						// 리눅스 .so 에서는 필요 없음
+{
+	return new Calc;
+}
+
+// Cient 개발자는
+// 1. DLL Load
+// 2. DLL 안에 있는 약속된 함수("create") 호출하면 Proxy 생성!
+
+
+
+
+
