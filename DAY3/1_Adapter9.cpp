2e616b0f17fe601764d8f72ace97acef40e193f2
ecourse <smkang@codenuri.co.kr>
2026-08-26 10:15:22 +0900

    Add files via upload

--- DAY3/1_Adapter9.cpp
+++ DAY3/1_Adapter9.cpp
@@ -7,7 +7,11 @@
 
 int main()
 {
-	std::stack<int> s1;
+	// C++ 표준에서는 "stack" 을 "stack adapter" 라고 부릅니다
+	// => cppreference.com 에 접속해 보세요
+	// => 1번째 화면에서 "container library" 선택
+
+	std::stack<int> s1;	// std::stack<int, std::deque<int>> s1;
 	std::stack<int, std::vector<int>> s2;
 	std::stack<int, std::list<int>> s3;
 
