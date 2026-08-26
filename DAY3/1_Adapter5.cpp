a1121bda6a26c5b2d7c600a5ba402f6e27edcbae
ecourse <smkang@codenuri.co.kr>
2026-08-26 09:46:28 +0900

    Add files via upload

---
+++ DAY3/1_Adapter5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <list>
+#include <vector>
+#include <deque>
+
+// 상속을 사용한 stack adapter
+template<typename T> 
+class stack : public std::list<T>
+{
+public:
+	void push(const T& e) { std::list<T>::push_back(e); }
+	void pop()            { std::list<T>::pop_back(); }
+	T& top()			  { return std::list<T>::back(); }
+};
+
+int main()
+{
+	stack<int> s;
+	s.push(10);
+	s.push(20);
+
+	auto s = s.top();
+} 
