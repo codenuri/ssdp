29fc4a4d11072135d03fdb00b829e859e97cae22
ecourse <smkang@codenuri.co.kr>
2026-08-27 13:53:02 +0900

    Add files via upload

---
+++ DAY4/5_Command5.cpp
@@ -0,0 +1,147 @@
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+
+class Shape
+{
+public:
+	virtual void draw() = 0;
+	virtual ~Shape() {}
+};
+
+class Rect : public Shape
+{
+public:
+	void draw() override { std::cout << "draw rect" << std::endl; }
+};
+class Circle : public Shape
+{
+public:
+	void draw() override { std::cout << "draw circle" << std::endl; }
+};
+
+
+struct ICommand
+{
+	virtual void execute() = 0;
+	virtual bool can_undo() { return false; }
+	virtual void undo() {}
+	virtual ~ICommand() {}
+};
+
+// 도형을 추가하는 명령은 동일한 코드가 많습니다.
+// => 기반 클래스에서 공통의 코드를 제공하고 변하는 것만 파생 클래스가 변경
+
+
+class AddCommand : public ICommand
+{
+	std::vector<Shape*>& v;
+public:
+	AddCommand(std::vector<Shape*>& v) :v(v) {}
+
+	void execute() override { v.push_back( create_shape() ); }
+	bool can_undo() { return true; }
+	void undo()
+	{
+		Shape* s = v.back();
+		v.pop_back();
+		delete s;
+	}
+
+	// Factory Method : 객체를 생성하기 위한 인터페이스를 제공하지만
+	//                  객체의 종류는 파생 클래스가 결정한다
+	virtual Shape* create_shape() = 0;
+};
+
+
+
+
+
+class AddRectCommand : public AddCommand
+{
+public:
+	AddRectCommand(std::vector<Shape*>& v) : AddCommand(v) {}
+
+	Shape* create_shape() { return new Rect; }
+};
+
+class AddCircleCommand : public AddCommand
+{
+public:
+	AddCircleCommand(std::vector<Shape*>& v) : AddCommand(v) {}
+
+	Shape* create_shape() { return new Circle; }
+};
+
+
+
+class DrawCommand : public ICommand
+{
+	std::vector<Shape*>& v;
+public:
+	DrawCommand(std::vector<Shape*>& v) :v(v) {}
+
+	void execute() override
+	{
+		for (auto s : v)
+			s->draw();
+	}
+
+	bool can_undo() { return true; }
+
+	void undo() { system("cls"); }
+};
+
+int main()
+{
+	std::vector<Shape*> v;
+
+	std::stack<ICommand*> cmd_stack; 
+
+	ICommand* command = nullptr;
+
+	while (1)
+	{
+		int cmd;
+		std::cin >> cmd;
+
+		if (cmd == 1)
+		{
+			command = new AddRectCommand(v);
+			command->execute();
+			cmd_stack.push(command);
+		}
+		else if (cmd == 2)
+		{
+			command = new AddCircleCommand(v);
+			command->execute();
+			cmd_stack.push(command);
+		}
+		else if (cmd == 9)
+		{
+			command = new DrawCommand(v);
+			command->execute();
+			cmd_stack.push(command);
+		}
+
+		else if (cmd == 0)
+		{
+			if (!cmd_stack.empty())
+			{
+				command = cmd_stack.top();
+				cmd_stack.pop();
+
+				if (command->can_undo())
+				{
+					command->undo();
+				}
+
+				delete command; 
+			}
+		}
+	}
+}
+
+
+
+
