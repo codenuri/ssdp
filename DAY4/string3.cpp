c1de27df7f4b5b650d0cab43ff80da39a1e18cc2
ecourse <smkang@codenuri.co.kr>
2026-08-27 09:24:12 +0900

    Add files via upload

--- DAY4/string3.cpp
+++ DAY4/string3.cpp
@@ -10,11 +10,17 @@ int main()
 	std::string s1 = "hello";
 
 	// 아래 2줄의 메모리 그림이 중요합니다.
-	std::string s2      = s1;
-	std::string_view sv1 = s1;
+	std::string s2      = s1;	// s2 가 s1의 문자열의 
+								// 복사본을 생성해서 소유
+	std::string_view sv1 = s1;	// sv 가 "s1.버퍼주소" 를 가리키는것
+								// 즉, const char*
 
 
 	// 아래 2줄의 메모리 그림도 알아야 합니다.
+	// 핵심 : string_view 가 직접 literal 로 초기화 될때
+
 	std::string s3       = "hello";
 	std::string_view sv2 = "hello";
+
+	// 참고 : std::string_view 는 읽기만 가능
 }
