32ba0bef58495eaff01ca4b697c6d566010dc1cc
ecourse <smkang@codenuri.co.kr>
2026-08-27 14:28:48 +0900

    Add files via upload

---
+++ DAY4/6_iterator4.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+
+template<typename T> struct Node
+{
+	T     data;
+	Node* next;
+
+	Node(const T& d, Node* n) : data(d), next(n) {}
+};
+
+
+// 모든 반복자가 지켜야 하는 규칙
+template<typename T> struct IIterator
+{
+	virtual bool hasNext() = 0;
+	virtual T& next() = 0;
+	virtual ~IIterator() {}
+};
+
+// 모든 컬렉션(컨테이너)가 지켜야 하는 규칙
+template<typename T> struct ICollection
+{
+	virtual IIterator<T>* iterator() = 0;
+	virtual ~ICollection() {}
+};
+
+
+
+
+
+
+template<typename T> struct slist
+{
+	Node<T>* head = 0;
+public:
+	void push_front(const T& a) { head = new Node<T>(a, head); }
+};
+
+int main()
+{
+	slist<int> s;
+	s.push_front(10);
+	s.push_front(20);
+	s.push_front(30);
+	s.push_front(40);
+}
