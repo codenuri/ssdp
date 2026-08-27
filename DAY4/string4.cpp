815d3bf2747e505155958e6e97d5b55f6fb1ff39
ecourse <smkang@codenuri.co.kr>
2026-08-27 09:26:10 +0900

    Add files via upload

---
+++ DAY4/string4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+#include <string_view>
+
+// 인자로 받는 문자열을 읽기만 하려고 합니다
+// 다음중 가장 좋은 코드는 ?
+
+void f1(std::string s)        {}
+void f2(const std::string& s) {}
+void f3(std::string_view s)   {}
+
+int main()
+{
+	std::string s = "hello";
+	f1(s);
+	f2(s);
+	f3(s);
+
+	f1("hello");
+	f2("hello");
+	f3("hello");
+}
