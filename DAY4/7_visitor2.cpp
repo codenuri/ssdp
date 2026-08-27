2ae9ae256a978f30ef273ed3154bf33fc08cd863
ecourse <smkang@codenuri.co.kr>
2026-08-27 16:03:03 +0900

    Add files via upload

--- DAY4/7_visitor2.cpp
+++ DAY4/7_visitor2.cpp
@@ -12,7 +12,7 @@ struct IVisitor
 	virtual ~IVisitor() {}
 };
 
-// 방문자 핵심 : 자신이 방문하는 요소 한개 에 대한 연산을 정의하는 클래스
+// 방문자 핵심 : 자신이 방문하는 "요소 한개 에 대한 연산을 정의" 하는 클래스
 template<typename T>
 class TwiceVisitor : public IVisitor<T>
 {
@@ -25,9 +25,23 @@ class ShowVisitor : public IVisitor<T>
 public:
 	void visit(T& e) { std::cout << e << std::endl; }
 };
+template<typename T>
+class ZeroVisitor : public IVisitor<T>
+{
+public:
+	void visit(T& e) { e = 0; }
+};
+
+// 모든 방문의 대상은 accept 가 있어야 한다.
+// => 인터페이스로 약속
+template<typename T> struct IAcceptor
+{
+	virtual void accept(IVisitor<T>* visitor) = 0;
+	virtual ~IAcceptor() {}
+};
 
 // 표준의 std::list 를 상속해서 방문자 패턴(accept) 를 구현해 봅시다.
-template<typename T> class MyList : public std::list<T>
+template<typename T> class MyList : public std::list<T>, public IAcceptor<T>
 {
 public:
 	using std::list<T>::list; // 생성자 상속. list 의 모든 생성자를 사용할수있게
@@ -50,6 +64,10 @@ int main()
 
 	TwiceVisitor<int> tv;  // 방문하는 요소를 2배로
 	s.accept(&tv);	
+
+	ZeroVisitor<int> zv;  // 방문하는 요소를 0 으로
+	s.accept(&zv);
+
 
 	ShowVisitor<int> sv;	// 방문하는 요소를 출력
 	s.accept(&sv);
