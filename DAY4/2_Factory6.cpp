141b6cf5b05057c884e52e88be383a618a8c2c92
ecourse <smkang@codenuri.co.kr>
2026-08-27 11:06:55 +0900

    Add files via upload

---
+++ DAY4/2_Factory6.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+#include "singleton.h"
+
+class Shape
+{
+public:
+	virtual void draw() = 0;
+	virtual ~Shape() {}
+};
+
+class Rect : public Shape
+{
+public:
+	void draw() override { std::cout << "draw Rect" << std::endl; }
+
+	static Shape* create() { return new Rect; }
+};
+
+
+
+
+
+
+class Circle : public Shape
+{
+public:
+	void draw() override { std::cout << "draw Circle" << std::endl; }
+
+	static Shape* create() { return new Circle; }
+};
+
+
+class ShapeFactory
+{
+	MAKE_SINGLETON(ShapeFactory)
+
+		using CREATOR = Shape * (*)();
+
+	std::map<int, CREATOR> create_map; 
+
+public:
+
+	void register_shape(int type, CREATOR c)
+	{
+		create_map[type] = c;
+	}
+
+
+	Shape* create(int type)
+	{
+		Shape* s = nullptr;
+
+		auto it = create_map.find(type);
+
+		if (it != create_map.end())
+		{
+			s = it->second(); 
+		}
+		return s;
+	}
+
+};
+
+
+int main()
+{
+	ShapeFactory& factory = ShapeFactory::get_instance();
+
+	factory.register_shape(1, &Rect::create);
+	factory.register_shape(2, &Circle::create);
+
+
+	std::vector<Shape*> v;
+
+	while (1)
+	{
+		int cmd;
+		std::cin >> cmd;
+
+		if (cmd >= 1 && cmd <= 7)
+		{
+			Shape* s = factory.create(cmd);
+
+			if (s != nullptr)
+				v.push_back(s);
+		}
+
+		else if (cmd == 9)
+		{
+			for (int i = 0; i < v.size(); i++)
+			{
+				v[i]->draw();
+			}
+		}
+	}
+}
+
+
+
+
