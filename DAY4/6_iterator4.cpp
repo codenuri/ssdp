55ed54643273f851f04398167fb80f41defbd018
ecourse <smkang@codenuri.co.kr>
2026-08-27 14:38:56 +0900

    Add files via upload

--- DAY4/6_iterator4.cpp
+++ DAY4/6_iterator4.cpp
@@ -26,25 +26,50 @@ template<typename T> struct ICollection
 };
 //---------------------------------
 // 아래 코드가 실제 slist 의 iterator 구현 코드
-template<typename T> class slist_iterator : public IIterator<T>
+template<typename T>
+class slist_iterator : public IIterator<T>
 {
+	Node<T>* current = nullptr;
 public:
+	slist_iterator(Node<T>* p = nullptr)
+		: current(p) {}
+
+	// 이제 약속된 방법으로 다음으로 이동만 하면됩니다.
+
+	T& next() override
+	{
+		T& tmp = current->data;
+		current = current->next;
+		return tmp;
+	}
+
+	bool hasNext() override
+	{
+		return current != nullptr;
+	}
 };
+/*
+slist_iterator<int> p(400번지);
+while (p.hasNext())
+{
+	int n = p.next();
+}
+*/
 
+//--------------------------
+// 모든 컬렉션은 "iterator()" 함수가 있어야 한다
+// => ICollection 인터페이스를 구현해야 한다
 
-
-
-
-
-
-
-
-
-template<typename T> struct slist
+template<typename T> struct slist : public ICollection<T>
 {
 	Node<T>* head = 0;
 public:
 	void push_front(const T& a) { head = new Node<T>(a, head); }
+
+	IIterator<T>* iterator()
+	{
+		return new slist_iterator<T>(head);
+	}
 };
 
 int main()
@@ -54,4 +79,16 @@ int main()
 	s.push_front(20);
 	s.push_front(30);
 	s.push_front(40);
+
+	// 아래 코드가 실제 Java 에서 iterator 사용하는 방법과 거의 동일합니다.
+
+	auto p = s.iterator();
+
+	while (p->hasNext())
+	{
+		std::cout << p->next() << std::endl;
+	}
 }
+
+
+
