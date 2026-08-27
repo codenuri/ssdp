8efa9d132976208dce43d73b609c6f192cbd35a5
ecourse <smkang@codenuri.co.kr>
2026-08-28 08:57:42 +0900

    Add files via upload

---
+++ DAY5/2_GUI2.cpp
@@ -0,0 +1,20 @@
+﻿#define USING_GUI
+#include "cppmaster.h"
+
+int foo(int hwnd, int msg, int a, int b)
+{
+	switch (msg)
+	{
+	case WM_LBUTTONDOWN: std::cout << "LBUTTON " << hwnd << std::endl; break;
+	case WM_KEYDOWN:     std::cout << "KEYDOWN " << hwnd << std::endl; break;
+	}
+	return 0;
+}
+
+int main()
+{
+	int h1 = ec_make_window(foo, "A");   
+
+	ec_process_message();
+}
+
