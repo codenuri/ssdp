a620d336929f5f9f92a645e5457b8ad6ec0d47b6
ecourse <smkang@codenuri.co.kr>
2026-08-27 13:43:07 +0900

    Add files via upload

--- DAY4/5_Command4.cpp
+++ DAY4/5_Command4.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <vector>
+#include <stack>
 
 class Shape
 {
@@ -36,30 +37,83 @@ class AddRectCommand : public ICommand
 public:
 	AddRectCommand(std::vector<Shape*>& v) :v(v) {}
 
-	void execute() override {}
-	bool can_undo() { return false; }
-	void undo() {}
+	void execute() override { v.push_back(new Rect); }
+
+	bool can_undo() { return true; }
+
+	void undo() 
+	{
+		Shape* s = v.back();
+		v.pop_back();
+		delete s;
+	}
 };
 
+class AddCircleCommand : public ICommand
+{
+	std::vector<Shape*>& v;
+public:
+	AddCircleCommand(std::vector<Shape*>& v) :v(v) {}
 
+	void execute() override { v.push_back(new Circle); }
 
+	bool can_undo() { return true; }
 
+	void undo()
+	{
+		Shape* s = v.back();
+		v.pop_back();
+		delete s;
+	}
+};
+
+class DrawCommand : public ICommand
+{
+	std::vector<Shape*>& v;
+public:
+	DrawCommand(std::vector<Shape*>& v) :v(v) {}
+
+	void execute() override 
+	{ 
+		for (auto s : v)
+			s->draw();
+	}
+
+	bool can_undo() { return true; }
+
+	void undo() { system("cls");}
+};
 
 int main()
 {
 	std::vector<Shape*> v;
+
+	std::stack<ICommand*> cmd_stack; // undo 를 위해서 모든 명령을 보관
+
+	ICommand* command = nullptr;
 
 	while (1)
 	{
 		int cmd;
 		std::cin >> cmd;
 
-		if (cmd == 1) v.push_back(new Rect);
-		else if (cmd == 2) v.push_back(new Circle);
+		if (cmd == 1)
+		{
+			command = new AddRectCommand(v);
+			command->execute();
+			cmd_stack.push(command);
+		}
+		else if (cmd == 2)
+		{
+			command = new AddCircleCommand(v);
+			command->execute();
+			cmd_stack.push(command);
+		}
 		else if (cmd == 9)
 		{
-			for (auto s : v)
-				s->draw();
+			command = new DrawCommand(v);
+			command->execute();
+			cmd_stack.push(command);
 		}
 	}
 }
