d9e3c07d795c91eb55da876d869e5aef3b94364d
ecourse <smkang@codenuri.co.kr>
2026-08-28 14:26:14 +0900

    Add files via upload

--- DAY5/7_mediator1.cpp
+++ DAY5/7_mediator1.cpp
@@ -1,6 +1,10 @@
 ﻿#include <iostream>
 #include <conio.h>
 
+// 움직임을 포착하는 센서
+// 빛을 감지해서 점심/저녁을 구별하는 센서 
+
+// 모든 센서 클래스의 기반 클래스
 class Device
 {
 public:
@@ -12,10 +16,13 @@ class LightSensor : public Device
 	bool dark = false;
 public:
 	bool get_dark() const { return dark; }
+
 	void set_dark(bool dark)
 	{
 		std::cout << "Environment: " << (dark ? "dark" : "bright") << '\n';
 		this->dark = dark;
+
+		// 센서 상태가 변경되면 약속된 가상함수 호출
 		changed();
 	}
 };
@@ -29,6 +36,8 @@ public:
 	{
 		std::cout << "Motion: " << (detected ? "detected" : "none") << '\n';
 		this->detected = detected;
+
+		// 상태가 변경되면 약속된 가상함수 호출
 		changed();
 	}
 };
