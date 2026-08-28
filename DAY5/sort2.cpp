100f49b4c361640ff4ed2277fcfe22063a8702eb
ecourse <smkang@codenuri.co.kr>
2026-08-28 15:51:36 +0900

    Add files via upload

---
+++ DAY5/sort2.cpp
@@ -0,0 +1,22 @@
+﻿#include <algorithm>
+#include <vector>
+#include <string>
+#include <print>
+
+int main()
+{
+	std::vector<std::string> v = { "AA", "B", "CCC" };
+
+	// a, b 비교 필요하면
+	// 2번째인자함수(3번째인자함수(a), 3번째인자함수(b))
+	/*
+	std::ranges::sort(v, 
+					  [](int a, int b) { return a < b; },
+					  [](const std::string& s) { return s.size(); });
+					  */
+
+	// v안의 모든 요소를 사전순서가 아닌 문자열 길이로 정렬한것
+	std::ranges::sort(v, {}, &std::string::size );
+
+	std::println("{}", v); // C++23 출력.  vector 를 바로 출력 가능
+}
