c9842f20a6ac02ae3f8776338343bcc1234279d6
ecourse <smkang@codenuri.co.kr>
2026-08-28 10:14:51 +0900

    Add files via upload

--- DAY5/2_GUI7.cpp
+++ DAY5/2_GUI7.cpp
@@ -10,7 +10,8 @@
 // => 모두 가상함수로 하면 "가상함수 테이블이 커집니다."
 // => 추가로 호출의 오버헤드도 약간 있습니다.
 
-// #2. 
+// #2. 다른 방법으로 해봅시다 => 다음 소스에서 구현
+// => 현재소스에서는 아래 "self->lbutton_down()" 의 주석만 알아 두세요
 
 class Window
 {
@@ -34,7 +35,13 @@ public:
 		switch (msg)
 		{
 		case WM_LBUTTONDOWN:
-			self->lbutton_down();
+			self->lbutton_down(); // 멤버 함수 호출의 원리
+								  // => self 가 Window* 이므로 Window 클래스를 조사해서
+								  //    lbutton_down() 이 virtual 인지 아닌지 확인
+								  // (1) virtual 이 아니면 static binding
+								  //     => 원리, 실제 객체를 조사하지 않고 포인터 타입만으로 호출
+								  //     => self 가 Window* 이므로 Window::lbutton_down() 호출
+								  // (2) virtual 이면 dynamic binding
 			break;
 
 		case WM_KEYDOWN:
@@ -51,14 +58,7 @@ public:
 class MainFrame : public Window
 {
 public:
-	void lbutton_down() 
-	{
-		std::cout << "MainFrame lbutton_down\n";
-	}
-	void key_down()
-	{
-		std::cout << "MainFrame key_down\n";
-	}
+	void lbutton_down() { std::cout << "MainFrame lbutton_down\n";}
 };
 
 int main()
