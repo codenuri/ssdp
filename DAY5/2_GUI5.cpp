61fd9c219892c1d04a342997e10666c776223fc6
ecourse <smkang@codenuri.co.kr>
2026-08-28 09:47:01 +0900

    Add files via upload

--- DAY5/2_GUI5.cpp
+++ DAY5/2_GUI5.cpp
@@ -1,13 +1,31 @@
 ﻿#define USING_GUI
 #include "cppmaster.h"
 #include <map>
+#include <vector>
 
 class Window
 {
 	int handle;
 	inline static std::map<int, Window*> this_map;
 
+	Window* parent = nullptr; // 부모 윈도우는 한개!
+	std::vector<Window*> child_vector; // 자식 윈도우는 여러개
 public:
+
+	void add_child(Window* child)
+	{
+		// 객체간 포인터 관계 설정
+		child->parent = this;
+		child_vector.push_back(child);
+
+		// C 함수로 실제 GUI 에 자식윈도우로 부착
+		ec_add_child(this->handle, child->handle);
+	}
+
+
+
+
+
 	void create(const std::string& title)
 	{
 		handle = ec_make_window(&foo, title);
