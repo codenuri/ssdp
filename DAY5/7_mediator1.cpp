da595677a9d72a01d819027b0464638ab2d2e5f4
ecourse <smkang@codenuri.co.kr>
2026-08-28 14:22:57 +0900

    Add files via upload

---
+++ DAY5/7_mediator1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <conio.h>
+
+class Device
+{
+public:
+	virtual void changed() {}
+};
+
+class LightSensor : public Device
+{
+	bool dark = false;
+public:
+	bool get_dark() const { return dark; }
+	void set_dark(bool dark)
+	{
+		std::cout << "Environment: " << (dark ? "dark" : "bright") << '\n';
+		this->dark = dark;
+		changed();
+	}
+};
+
+class MotionSensor : public Device
+{
+	bool detected = false;
+public:
+	bool get_motion() const { return detected; }
+	void set_motion(bool detected)
+	{
+		std::cout << "Motion: " << (detected ? "detected" : "none") << '\n';
+		this->detected = detected;
+		changed();
+	}
+};
+
+int main()
+{
+	LightSensor ls;
+	MotionSensor ms;
+
+	ls.set_dark(true);		// 어두워 졌다
+	ms.set_motion(true);	// 움직임이 포착
+}
