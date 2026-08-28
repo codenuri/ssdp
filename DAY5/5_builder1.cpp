265c257972fa183a54651b7c6045573ee024fa56
ecourse <smkang@codenuri.co.kr>
2026-08-28 13:44:46 +0900

    Add files via upload

---
+++ DAY5/5_builder1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+class Car
+{
+public:
+    std::string engine;
+    std::string tire;
+    std::string interior;
+
+    void show() const
+    {
+        std::cout << engine << '\n'
+            << tire << '\n'
+            << interior << '\n';
+    }
+};
+
+int main()
+{
+    // 비싼차
+    Car c1;
+    c1.engine = "best engine";
+    c1.tire   = "best tire";
+    c1.interior = "best interior";
+
+    // 중형차
+    Car c2;
+    c2.engine = "good engine";
+    c2.tire = "good tire";
+    c2.interior = "good interior";
+
+}
