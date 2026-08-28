7bde95af085ad6d94442d353786cde3e385e9f65
ecourse <smkang@codenuri.co.kr>
2026-08-28 13:19:25 +0900

    Add files via upload

--- DAY5/4_state3.cpp
+++ DAY5/4_state3.cpp
@@ -14,10 +14,20 @@ public:
 	virtual void do_attack() { std::cout << "attack" << std::endl; }
 };
 
+class SuperCharacter : public Character
+{
+public:
+	void do_run()    { std::cout << "fast run" << std::endl; }
+	void do_attack() { std::cout << "power attack" << std::endl; }
+};
 
 int main()
 {
 	Character* p = new Character;
+	p->run();
+	p->attack();
+
+	p = new SuperCharacter; // ?
 	p->run();
 	p->attack();
 }
