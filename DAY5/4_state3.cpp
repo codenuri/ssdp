361b4f2aa9a480069a6eb0af0b04bfca50a6c62f
ecourse <smkang@codenuri.co.kr>
2026-08-28 13:17:34 +0900

    Add files via upload

---
+++ DAY5/4_state3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+// 방법 #2. 변하는 것을 가상함수로
+
+class Character
+{
+	int gold;
+	int item = 1;
+public:
+	void run()    { do_run(); }
+	void attack() { do_attack(); }
+		
+	virtual void do_run()    { std::cout << "run" << std::endl; }
+	virtual void do_attack() { std::cout << "attack" << std::endl; }
+};
+
+
+int main()
+{
+	Character* p = new Character;
+	p->run();
+	p->attack();
+}
+
+
+
