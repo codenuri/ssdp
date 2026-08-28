d7cd38ae660114bfe1fe50c492ad8557bb571b4c
ecourse <smkang@codenuri.co.kr>
2026-08-28 13:22:43 +0900

    Add files via upload

--- DAY5/4_state3.cpp
+++ DAY5/4_state3.cpp
@@ -23,11 +23,16 @@ public:
 
 int main()
 {
-	Character* p = new Character;
+	Character* p = new Character;	// 여기서 캐릭터 객체 생성 - "A"
 	p->run();
 	p->attack();
 
-	p = new SuperCharacter; // ?
+	p = new SuperCharacter; // 이 코드는 "A" 의 동작이 변경된 것이 아님
+							// 다른 동작을 가진 새로운 객체를 만든것
+							// 원하는 것 : 동일 객체의 모든 동작을 변경
+							// 현재 코드 : 새로운 객체의 생성(변경)
+							// => 동일 객체의 동작만 전체를 변경할수 없을까?
+							// => state 패턴. 다음 예제
 	p->run();
 	p->attack();
 }
