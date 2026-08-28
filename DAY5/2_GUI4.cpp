ac5b99b3bd4fa4b2567297eefda73d2a2ebfc067
ecourse <smkang@codenuri.co.kr>
2026-08-28 09:12:10 +0900

    Add files via upload

---
+++ DAY5/2_GUI4.cpp
@@ -0,0 +1,48 @@
+﻿#define USING_GUI
+#include "cppmaster.h"
+
+
+
+
+class Window
+{
+	int handle;
+public:
+	void create(const std::string& title)
+	{
+		handle = ec_make_window(&foo, title);
+	}
+
+	static int foo(int hwnd, int msg, int a, int b)
+	{
+		switch (msg)
+		{
+		case WM_LBUTTONDOWN: 
+			lbutton_down(); // 왜 에러일까 ?
+			break;
+
+		case WM_KEYDOWN:    
+			key_down();     
+			break;
+		}
+		return 0;
+	}
+	
+	virtual void lbutton_down() {}
+	virtual void key_down() {}
+};
+
+int main()
+{
+	Window w;
+	w.create("Hello");
+
+	ec_process_message();
+}
+
+
+
+
+
+
+
